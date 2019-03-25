9aa796b000a3c0d53c438fb124c2986277c4997a
Nathan Levesque <nlevesq@mit.edu>
2019-03-25 14:51:36 -0400

    Added model for edX tokens

--- courseware/models.py
+++ courseware/models.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.db import models
 
 from mitxpro.models import TimestampedModel
+
 from courseware.constants import COURSEWARE_PLATFORM_CHOICES, PLATFORM_EDX
 
 
@@ -20,3 +21,16 @@ class CoursewareUser(TimestampedModel):
 
     class Meta:
         unique_together = ("user", "platform")
+
+
+class OpenEdxApiAuth(TimestampedModel):
+    """Model that stores OAuth2 tokens for authenticating Open edX API calls"""
+
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    refresh_token = models.CharField(max_length=128)
+    access_token = models.CharField(null=True, max_length=128)
+    access_token_expires_on = models.DateTimeField(null=True)
+
+    class Meta:
+        index_together = ("user", "access_token_expires_on")
