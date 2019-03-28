9d37dabb3cae4c32dbdaf8c883e23a670ef45591
George Schneeloch <gschneel@mit.edu>
2019-03-28 17:08:00 -0400

    Remove v0 from API

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from ecommerce.views import CheckoutView, OrderFulfillmentView
 
 urlpatterns = [
-    url(r"^api/v0/checkout/$", CheckoutView.as_view(), name="checkout"),
+    url(r"^api/checkout/$", CheckoutView.as_view(), name="checkout"),
     url(
-        r"^api/v0/order_fulfillment/$",
+        r"^api/order_fulfillment/$",
         OrderFulfillmentView.as_view(),
         name="order-fulfillment",
     ),
