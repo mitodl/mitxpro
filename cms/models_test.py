7edb2b665c598d26616f59dd634e4fe788125001
Tasawer Nawaz <tasawer.nawaz@arbisoft.com>
2019-05-09 19:38:19 +0500

    make generic resource page in wagtail (#238)
    * make generic resource page in wagtail, it can be used to add pages like tos, about, privay
    
    * test added
    
    * feedback addressed
    
    * linting

---
+++ cms/models_test.py
@@ -0,0 +1,46 @@
+""" Tests for cms pages. """
+
+import json
+import pytest
+
+from cms.factories import ResourcePageFactory
+
+pytestmark = [pytest.mark.django_db]
+
+
+def test_resource_page():
+    """
+    Verify user can create resource page.
+    """
+    page = ResourcePageFactory.create(
+        title="title of the page",
+        sub_heading="sub heading of the page",
+        content=json.dumps(
+            [
+                {
+                    "type": "content",
+                    "value": {
+                        "heading": "Introduction",
+                        "detail": "details of introduction",
+                    },
+                }
+            ]
+        ),
+    )
+
+    assert page.title == "title of the page"
+    assert page.sub_heading == "sub heading of the page"
+
+    for block in page.content:  # pylint: disable=not-an-iterable
+        assert block.block_type == "content"
+        assert block.value["heading"] == "Introduction"
+        assert block.value["detail"].source == "details of introduction"
+
+
+def test_resource_page_unique_slug():
+    """
+    Verify that if user creates pages with same title, there slug would be auto generated uniquely.
+    """
+    page = ResourcePageFactory(title="title of the page")
+    another_page = ResourcePageFactory(title="title of the page")
+    assert page.slug != another_page.slug
