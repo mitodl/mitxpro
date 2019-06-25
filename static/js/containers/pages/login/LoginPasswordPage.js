16eda2b1f225841fe56031ff529235eabdf02e55
tasawernawaz <tasawer.nawaz@arbisoft.com>
2019-06-25 17:57:07 +0500

    fix redirect url after signin

--- static/js/containers/pages/login/LoginPasswordPage.js
+++ static/js/containers/pages/login/LoginPasswordPage.js
@@ -50,11 +50,11 @@ class LoginPasswordPage extends React.Component<Props> {
 
     try {
       const {
-        body: { state, redirectUrl, errors }
+        body: { state, redirect_url, errors }
       }: { body: AuthResponse } = await loginPassword(password, partialToken)
 
       if (state === STATE_SUCCESS) {
-        window.location.href = redirectUrl || routes.dashboard
+        window.location.href = redirect_url || routes.dashboard
       } else if (errors.length > 0) {
         setErrors({
           password: errors[0]
