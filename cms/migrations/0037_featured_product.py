037735804cfca76d681b4b86c30cead901e938fd
tasawernawaz <tasawer.nawaz@arbisoft.com>
2019-07-04 14:08:46 +0500

    fix migrations

--- cms/migrations/0037_featured_product.py
+++ cms/migrations/0037_featured_product.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [("cms", "0035_alter_helptext_video_url")]
+    dependencies = [("cms", "0036_make_subhead_optional")]
 
     operations = [
         migrations.AddField(
