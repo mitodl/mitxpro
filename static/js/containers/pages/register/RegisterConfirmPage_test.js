1151a9bbd895e3c9048bab2fe6ed24fc554276af
George Schneeloch <gschneel@mit.edu>
2019-07-08 10:14:47 -0400

    Add notification when user verifies their email (#760)
    * Add notification when user verifies their email
    
    * Flow

---
+++ static/js/containers/pages/register/RegisterConfirmPage_test.js
@@ -0,0 +1,59 @@
+// @flow
+import { assert } from "chai"
+
+import IntegrationTestHelper from "../../../util/integration_test_helper"
+import RegisterConfirmPage, {
+  RegisterConfirmPage as InnerRegisterConfirmPage
+} from "./RegisterConfirmPage"
+import { STATE_REGISTER_DETAILS } from "../../../lib/auth"
+
+describe("RegisterConfirmPage", () => {
+  let helper, renderPage
+
+  beforeEach(() => {
+    helper = new IntegrationTestHelper()
+    renderPage = helper.configureHOCRenderer(
+      RegisterConfirmPage,
+      InnerRegisterConfirmPage,
+      {},
+      {
+        location: {
+          search: ""
+        }
+      }
+    )
+  })
+
+  afterEach(() => {
+    helper.cleanup()
+  })
+
+  it("shows a message when the confirmation page is displayed and redirects", async () => {
+    helper.handleRequestStub.returns({})
+    const token = "asdf"
+    const { inner, store } = await renderPage({
+      entities: {
+        auth: {
+          state:        STATE_REGISTER_DETAILS,
+          partialToken: token,
+          extraData:    {
+            name: "name"
+          }
+        }
+      }
+    })
+
+    inner.instance().componentDidUpdate({}, {})
+    assert.deepEqual(store.getState().ui.userNotifications, {
+      "email-verified": {
+        type:  "text",
+        props: {
+          text:
+            "Success! We've verified your email. Please finish your account creation below."
+        }
+      }
+    })
+    assert.equal(helper.currentLocation.pathname, "/create-account/details/")
+    assert.equal(helper.currentLocation.search, `?partial_token=${token}`)
+  })
+})
