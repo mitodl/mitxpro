faa6b0220de358169321a90808f163d0d6264b9c
Matt Bertrand <mattbert@mit.edu>
2019-07-10 16:47:10 -0400

    Vouchers for django admin

--- voucher/admin.py
+++ voucher/admin.py
@@ -1,3 +1,30 @@
 """
 Admin bindings for voucher
 """
+from django.contrib import admin
+
+from voucher.models import Voucher
+
+
+class VoucherAdmin(admin.ModelAdmin):
+    """Admin view for vouchers"""
+
+    list_display = (
+        "id",
+        "course_id_input",
+        "course_title_input",
+        "course_start_date_input",
+        "employee_name",
+    )
+    search_fields = (
+        "user__username",
+        "user__email",
+        "product__id",
+        "employee_name",
+        "course_id_input",
+        "course_title_input",
+    )
+    readonly_fields = ("coupon", "product", "enrollment")
+
+
+admin.site.register(Voucher, VoucherAdmin)
