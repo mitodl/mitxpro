ad31680a5e24e566bd6d5e12f6d998f4df330257
George Schneeloch <gschneel@mit.edu>
2019-07-12 14:50:52 -0400

    Update some JS dependencies

--- static/js/babelhook.js
+++ static/js/babelhook.js
@@ -4,10 +4,10 @@ babelSharedLoader.query.presets = ["@babel/env", "@babel/preset-react"]
 
 require("@babel/polyfill")
 
-// window and global must be defined here before React is imported
-require("jsdom-global")(undefined, {
-  url: "http://fake/"
-})
+// jsdom initialization here adapted from from https://airbnb.io/enzyme/docs/guides/jsdom.html
+const { JSDOM } = require("jsdom")
+const jsdom = new JSDOM("<!doctype html><html><body></body></html>")
+const { window } = jsdom
 
 const { polyfill } = require("raf")
 polyfill(global)
@@ -17,13 +17,33 @@ polyfill(window)
 window.crypto = require("@trust/webcrypto")
 
 // We need to explicitly change the URL when window.location is used
-const changeURL = require("jsdom/lib/old-api").changeURL
+
+function copyProps(src, target) {
+  Object.defineProperties(target, {
+    ...Object.getOwnPropertyDescriptors(src),
+    ...Object.getOwnPropertyDescriptors(target)
+  })
+}
+
+global.window = window
+global.document = window.document
+global.navigator = {
+  userAgent: "node.js"
+}
+global.requestAnimationFrame = function(callback) {
+  return setTimeout(callback, 0)
+}
+global.cancelAnimationFrame = function(id) {
+  clearTimeout(id)
+}
+copyProps(window, global)
+
 Object.defineProperty(window, "location", {
   set: value => {
     if (!value.startsWith("http")) {
       value = `http://fake${value}`
     }
-    changeURL(window, value)
+    jsdom.reconfigure({ url: value })
   }
 })
 
