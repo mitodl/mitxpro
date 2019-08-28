7e4d608cc605922b2f20709c125183dee9ca73dc
Peter Pinch <pdpinch+github@hawklake.com>
2019-08-28 08:20:07 -0400

    add raw_id_fields to ecommerce django admin (#1056)

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -107,6 +107,7 @@ class CouponVersionInline(admin.StackedInline):
 
     model = CouponVersion
     readonly_fields = get_field_names(CouponVersion)
+    raw_id_fields = ("coupon", "payment_version")
     extra = 0
     show_change_link = True
     can_delete = False
@@ -120,6 +121,7 @@ class CouponAdmin(admin.ModelAdmin):
     list_display = ("id", "coupon_code", "get_payment_name")
     search_fields = ("coupon_code", "payment__name")
     list_filter = ("payment",)
+    raw_id_fields = ("payment",)
 
     model = Coupon
     save_on_top = True
@@ -152,6 +154,7 @@ class CouponPaymentVersionAdmin(admin.ModelAdmin):
     save_as = True
     save_as_continue = False
     save_on_top = True
+    raw_id_fields = ("payment",)
 
     def has_delete_permission(self, request, obj=None):
         return False
@@ -167,6 +170,7 @@ class CouponVersionAdmin(admin.ModelAdmin):
     save_as = True
     save_as_continue = False
     save_on_top = True
+    raw_id_fields = ("coupon", "payment_version")
 
     def has_delete_permission(self, request, obj=None):
         return False
@@ -179,6 +183,7 @@ class CouponSelectionAdmin(admin.ModelAdmin):
     """Admin for CouponSelections"""
 
     model = CouponSelection
+    raw_id_fields = ("coupon", "basket")
 
 
 class CouponEligibilityAdmin(admin.ModelAdmin):
@@ -187,7 +192,7 @@ class CouponEligibilityAdmin(admin.ModelAdmin):
     list_display = ("id", "coupon", "product")
     search_fields = ("coupon__coupon_code", "coupon__payment__name")
     list_filter = ("product",)
-    raw_id_fields = ("coupon",)
+    raw_id_fields = ("coupon", "product")
 
     model = CouponEligibility
 
@@ -203,6 +208,7 @@ class CouponRedemptionAdmin(admin.ModelAdmin):
     """Admin for CouponRedemptions"""
 
     model = CouponRedemption
+    raw_id_fields = ("coupon_version", "order")
 
 
 class ProductVersionAdmin(admin.ModelAdmin):
@@ -244,6 +250,7 @@ class DataConsentUserAdmin(admin.ModelAdmin):
 
     list_display = ("id", "user", "created_on")
     search_fields = ("user__username", "user__email")
+    raw_id_fields = ("user", "coupon")
 
     model = DataConsentUser
 
@@ -252,6 +259,7 @@ class DataConsentUserInline(admin.StackedInline):
     """Admin Inline for DataConsentUser objects"""
 
     model = DataConsentUser
+    raw_id_fields = ("user", "coupon")
     extra = 1
     show_change_link = True
 
@@ -262,6 +270,7 @@ class DataConsentAgreementAdmin(admin.ModelAdmin):
     list_filter = ("company",)
     list_display = ("id", "company", "created_on")
     search_fields = ("company", "content")
+    raw_id_fields = ("courses",)
     inlines = [DataConsentUserInline]
 
     model = DataConsentAgreement
