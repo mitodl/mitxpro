f8be9f586bb9df6cf4d96e5364bc81eef0045530
Ahmed Belal <ahmed.belal@arbisoft.com>
2019-08-28 14:11:18 +0500

    #874 Course run certificate management command
    Settings addition to app.json
    Added checks for valid grade value
    Revert "Settings addition to app.json"
    This reverts commit 15b112448befe4d64f905099fb5a7c7326d8ad82.
    App.json updated via command.
    Pylint nesting fixes and stuff
    Feedback
    Feedback and restructuring
    Command rename
    Feedback
    Last feedback

---
+++ courses/management/commands/sync_grades_and_certificates.py
@@ -0,0 +1,118 @@
+"""
+Management command to sync grades and certificates for a course run
+"""
+from django.core.management.base import BaseCommand, CommandError
+
+from courses.models import CourseRun
+from courses.utils import ensure_course_run_grade, process_course_run_grade_certificate
+from courseware.api import get_edx_grades_with_users
+from users.api import fetch_user
+
+
+class Command(BaseCommand):
+    """
+    Command to sync grades and certificates for a course run.
+    """
+
+    help = "Sync grades and certificates for a course run for all enrolled users or a specified user."
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--user",
+            type=str,
+            help="The id, email or username of the enrolled User",
+            required=False,
+        )
+        parser.add_argument(
+            "--run",
+            type=str,
+            help="The 'courseware_id' value for a CourseRun",
+            required=True,
+        )
+        parser.add_argument(
+            "--grade",
+            type=float,
+            help="Override a grade. Setting grade: 0 blacklists a user. Setting a passing grade \
+                (>0.0) whitelists a user. range: 0.0 - 1.0",
+            required=False,
+        )
+        parser.add_argument(
+            "--update",
+            action="store_true",
+            help="Update local grade records with results from the edX grades API",
+        )
+        super().add_arguments(parser)
+
+    def handle(self, *args, **options):  # pylint: disable=too-many-locals
+        """Handle command execution"""
+        # Grade override for all users for the course run. Disallowed.
+        if options["grade"] is not None and not options["user"]:
+            raise CommandError(
+                "No user supplied with override grade. Overwrite of grade is not supported for all users. Grade should only be supplied when a specific user is targeted."
+            )
+        try:
+            run = CourseRun.objects.get(courseware_id=options["run"])
+        except CourseRun.DoesNotExist:
+            raise CommandError(
+                "Could not find run with courseware_id={}".format(options["run"])
+            )
+
+        user = fetch_user(options["user"]) if options["user"] else None
+        override_grade = None
+        should_update = options["update"]
+
+        if options["grade"] is not None:
+            override_grade = float(options["grade"])
+            if override_grade and (override_grade < 0.0 or override_grade > 1.0):
+                raise CommandError("Invalid value for grade. Allowed range: 0.0 - 1.0")
+
+        edx_grade_user_iter = get_edx_grades_with_users(run, user=user)
+
+        results = []
+        for edx_grade, user in edx_grade_user_iter:
+            result = []
+            course_run_grade, created, updated = ensure_course_run_grade(
+                user=user,
+                course_run=run,
+                edx_grade=edx_grade,
+                should_update=should_update,
+            )
+
+            if created:
+                result.append("New grade record created")
+            elif updated:
+                result.append("Grade record updated with new data from edX grades API")
+
+            if override_grade is not None:
+                course_run_grade.grade = override_grade
+                course_run_grade.passed = bool(override_grade)
+                course_run_grade.letter_grade = None
+                course_run_grade.save_and_log(None)
+                result.append(
+                    "Grade overriden - new grade value: {}".format(
+                        course_run_grade.grade
+                    )
+                )
+
+            _, created, deleted = process_course_run_grade_certificate(
+                course_run_grade=course_run_grade
+            )
+
+            if deleted:
+                result.append("Certificate deleted")
+            else:
+                result.append(
+                    "Certificate created"
+                    if created
+                    else "Certificate ignored (already existed)"
+                )
+
+            result_summary = ", ".join(result) if len(result) else "No changes made"
+            results.append(
+                "Processed user {} ({}) in course run {}. Result: {}".format(
+                    user.username, user.email, run.courseware_id, result_summary
+                )
+            )
+
+        for result in results:
+            self.stdout.write(self.style.SUCCESS(result))
