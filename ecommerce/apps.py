fa4fd71184728245ba6e155607468dddcc7e0a3b
Matt Bertrand <mattbert@mit.edu>
2019-08-29 09:56:25 -0400

    Add id to Hubspot product title (#1053)

--- ecommerce/apps.py
+++ ecommerce/apps.py
@@ -6,3 +6,7 @@ class EcommerceConfig(AppConfig):
     """AppConfig for Ecommerce"""
 
     name = "ecommerce"
+
+    def ready(self):
+        """Application is ready"""
+        import ecommerce.signals  # pylint:disable=unused-import, unused-variable
