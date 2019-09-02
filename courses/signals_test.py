ba22ca33be49ec6700d61bb010a6a4425aed9e66
asadiqbal <aiqbal@edx.org>
2019-09-02 15:11:22 +0500

    Certificates: create program certificate

---
+++ courses/signals_test.py
@@ -0,0 +1,29 @@
+"""
+Tests for signals
+"""
+from unittest.mock import patch
+import pytest
+from courses.factories import CourseRunFactory, CourseRunCertificateFactory, UserFactory
+
+
+pytestmark = pytest.mark.django_db
+
+
+# pylint: disable=unused-argument
+@patch("courses.signals.transaction.on_commit", side_effect=lambda callback: callback())
+@patch("courses.signals.generate_program_certificate", autospec=True)
+def test_create_course_certificate(generate_program_cert_mock, mock_on_commit):
+    """
+    Test that generate_program_certificate is called when a course
+    certificate is created
+    """
+    user = UserFactory.create()
+    course_run = CourseRunFactory.create()
+    cert = CourseRunCertificateFactory.create(user=user, course_run=course_run)
+    generate_program_cert_mock.assert_called_once_with(
+        user, cert.course_run.course.program
+    )
+    cert.save()
+    generate_program_cert_mock.assert_called_once_with(
+        user, cert.course_run.course.program
+    )
