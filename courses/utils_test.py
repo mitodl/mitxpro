b542a7b4f3e71d048ecdf02c04239f7b7fc53cc6
Ahmed Belal <ahmed.belal@arbisoft.com>
2019-10-30 15:09:54 +0500

    #1265 Certificate generation only on passed status
    Typo fix
    
    Feedback

--- courses/utils_test.py
+++ courses/utils_test.py
@@ -10,8 +10,12 @@ from courses.factories import (
     CourseRunFactory,
     CourseRunCertificateFactory,
     ProgramCertificateFactory,
+    CourseRunGradeFactory,
 )
-from courses.utils import generate_program_certificate
+from courses.utils import (
+    generate_program_certificate,
+    process_course_run_grade_certificate,
+)
 from courses.models import ProgramCertificate
 
 pytestmark = pytest.mark.django_db
@@ -27,6 +31,80 @@ def user():
 def program():
     """User object fixture"""
     return ProgramFactory.create()
+
+
+@pytest.fixture()
+def course():
+    """Course object fixture"""
+    return CourseFactory.create()
+
+
+# pylint: disable=too-many-arguments
+@pytest.mark.parametrize(
+    "grade, passed, exp_certificate, exp_created, exp_deleted",
+    [
+        [0.25, True, True, True, False],
+        [0.0, True, False, False, False],
+        [1.0, False, False, False, False],
+    ],
+)
+def test_course_run_certificate(
+    user, course, grade, passed, exp_certificate, exp_created, exp_deleted
+):
+    """
+    Test that the certificate is generated correctly
+    """
+    certificate, created, deleted = process_course_run_grade_certificate(
+        CourseRunGradeFactory.create(
+            course_run__course=course, user=user, grade=grade, passed=passed
+        )
+    )
+    assert bool(certificate) is exp_certificate
+    assert created is exp_created
+    assert deleted is exp_deleted
+
+
+def test_course_run_certificate_idempotent(user, course):
+    """
+    Test that the certificate generation is idempotent
+    """
+    grade = CourseRunGradeFactory.create(
+        course_run__course=course, user=user, grade=0.25, passed=True
+    )
+
+    # Certificate is created the first time
+    certificate, created, deleted = process_course_run_grade_certificate(grade)
+    assert certificate
+    assert created
+    assert not deleted
+
+    # Existing certificate is simply returned without any create/delete
+    certificate, created, deleted = process_course_run_grade_certificate(grade)
+    assert certificate
+    assert not created
+    assert not deleted
+
+
+def test_course_run_certificate_blacklist(user, course):
+    """
+    Test that the certificate is not generated if grade isn't passing
+    """
+    grade = CourseRunGradeFactory.create(
+        course_run__course=course, user=user, grade=1.0, passed=True
+    )
+
+    # Initially the certificate is created
+    certificate, created, deleted = process_course_run_grade_certificate(grade)
+    assert certificate
+    assert created
+    assert not deleted
+
+    # Now that the grade indicates score 0.0, certificate should be deleted
+    grade.grade = 0.0
+    certificate, created, deleted = process_course_run_grade_certificate(grade)
+    assert not certificate
+    assert not created
+    assert deleted
 
 
 def test_generate_program_certificate_already_exist(user, program):
