eba6f2ab40b88976d05ef7d1032db767fe8cbaf7
Umar Asghar <mrumarasghar@gmail.com>
2019-10-31 17:21:02 +0500

    add unit test for multiple unused coupons

--- static/js/containers/App_test.js
+++ static/js/containers/App_test.js
@@ -85,4 +85,74 @@ describe("Top-level App", () => {
       assert.deepEqual(state.ui.userNotifications, expectedNotificationState)
     })
   })
+
+  describe("unused coupons alert", () => {
+    let userWithUnusedCoupons,
+      unusedCoupon,
+      unusedCoupon1,
+      unusedCoupon2,
+      expectedNotificationState
+
+    beforeEach(() => {
+      unusedCoupon = makeUnusedCoupon()
+      unusedCoupon1 = makeUnusedCoupon()
+      unusedCoupon2 = makeUnusedCoupon()
+      userWithUnusedCoupons = mergeRight(makeUser(), {
+        unused_coupons: [unusedCoupon, unusedCoupon1, unusedCoupon2]
+      })
+      expectedNotificationState = {
+        "unused-coupon-0": {
+          type:  ALERT_TYPE_UNUSED_COUPON,
+          props: {
+            productId:   unusedCoupon.product_id,
+            productName: unusedCoupon.product_name,
+            couponCode:  unusedCoupon.coupon_code
+          }
+        },
+        "unused-coupon-1": {
+          type:  ALERT_TYPE_UNUSED_COUPON,
+          props: {
+            productId:   unusedCoupon1.product_id,
+            productName: unusedCoupon1.product_name,
+            couponCode:  unusedCoupon1.coupon_code
+          }
+        },
+        "unused-coupon-2": {
+          type:  ALERT_TYPE_UNUSED_COUPON,
+          props: {
+            productId:   unusedCoupon2.product_id,
+            productName: unusedCoupon2.product_name,
+            couponCode:  unusedCoupon2.coupon_code
+          }
+        }
+      }
+    })
+
+    it("is triggered if the user is loaded and has unused coupons", async () => {
+      const { inner, store } = await renderPage()
+
+      inner.setProps({ currentUser: userWithUnusedCoupons })
+
+      const { ui } = store.getState()
+      assert.deepEqual(ui.userNotifications, expectedNotificationState)
+    })
+
+    it("is triggered if the user changes from the checkout page to another page", async () => {
+      let state
+      const { inner, store } = await renderPage(
+        {},
+        {
+          location: { pathname: routes.checkout }
+        }
+      )
+
+      inner.setProps({ currentUser: userWithUnusedCoupons })
+      state = store.getState()
+      assert.deepEqual(state.ui.userNotifications, {})
+
+      inner.setProps({ location: { pathname: routes.dashboard } })
+      state = store.getState()
+      assert.deepEqual(state.ui.userNotifications, expectedNotificationState)
+    })
+  })
 })
