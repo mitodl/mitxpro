e92126853b957437dc4e0eed423c4c547c298679
Ahmed Belal <ahmed.belal@arbisoft.com>
2019-11-05 17:35:43 +0500

    #1243 Set user context for Sentry

--- static/js/components/Header.js
+++ static/js/components/Header.js
@@ -1,5 +1,6 @@
 // @flow
 import React from "react"
+import * as Sentry from "@sentry/browser"
 
 import TopAppBar from "./TopAppBar"
 import NotificationContainer from "./NotificationContainer"
@@ -10,11 +11,27 @@ type Props = {
   currentUser: CurrentUser
 }
 
-const Header = ({ currentUser }: Props) => (
-  <React.Fragment>
-    <TopAppBar currentUser={currentUser} />
-    <NotificationContainer />
-  </React.Fragment>
-)
+const Header = ({ currentUser }: Props) => {
+  if (currentUser && currentUser.is_authenticated) {
+    Sentry.configureScope(scope => {
+      scope.setUser({
+        id:       currentUser.id,
+        email:    currentUser.email,
+        username: currentUser.username,
+        name:     currentUser.name
+      })
+    })
+  } else {
+    Sentry.configureScope(scope => {
+      scope.setUser(null)
+    })
+  }
+  return (
+    <React.Fragment>
+      <TopAppBar currentUser={currentUser} />
+      <NotificationContainer />
+    </React.Fragment>
+  )
+}
 
 export default Header
