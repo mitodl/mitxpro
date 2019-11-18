5c55ff39a6a4a13999067defa6173e6d16e1ace9
Doof <mitx-devops@mit.edu>
2019-11-18 15:29:20 +0000

    Release 0.32.0

--- mitxpro/settings.py
+++ mitxpro/settings.py
@@ -24,7 +24,7 @@ from mitxpro.envs import (
     OffsettingSchedule,
 )
 
-VERSION = "0.31.2"
+VERSION = "0.32.0"
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
