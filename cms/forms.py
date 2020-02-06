da26e840010c590a7abda834183a797ceb6c4cdb
Ahmed Belal <ahmed.belal@arbisoft.com>
2020-02-06 15:13:13 +0500

    Fixed vararg positioning

--- cms/forms.py
+++ cms/forms.py
@@ -9,7 +9,8 @@ class CertificatePageForm(WagtailAdminPageForm):
     Custom form for CertificatePage in order to filter course run IDs
     """
 
-    def __init__(self, *args, data=None, files=None, parent_page=None, **kwargs):
+    # pylint: disable=keyword-arg-before-vararg
+    def __init__(self, data=None, files=None, parent_page=None, *args, **kwargs):
         super().__init__(data, files, parent_page, *args, **kwargs)
         if parent_page.specific.is_course_page:
             self.fields["overrides"].block.child_blocks["course_run"].child_blocks[
