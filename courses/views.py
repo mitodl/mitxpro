0594e911ff0040b3ff33231afde1a1a54f134823
George Schneeloch <gschneel@mit.edu>
2020-02-10 12:32:04 -0500

    Remove unused CourseCatalogView (#1524)

--- courses/views.py
+++ courses/views.py
@@ -4,8 +4,6 @@ from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.db.models import Prefetch
-from django.views.generic import ListView
 
 from courses.models import Program, Course, CourseRun
 from courses.api import get_user_enrollments
@@ -16,8 +14,6 @@ from courses.serializers import (
     CourseRunEnrollmentSerializer,
     ProgramEnrollmentSerializer,
 )
-from courses.constants import DEFAULT_COURSE_IMG_PATH
-from mitxpro.views import get_base_context
 
 
 class ProgramViewSet(viewsets.ReadOnlyModelViewSet):
@@ -43,45 +39,6 @@ class CourseRunViewSet(viewsets.ReadOnlyModelViewSet):
 
     serializer_class = CourseRunSerializer
     queryset = CourseRun.objects.all()
-
-
-class CourseCatalogView(ListView):
-    """Course catalog view"""
-
-    template_name = "catalog.html"
-
-    def get_queryset(self):
-        sorted_courserun_qset = CourseRun.objects.order_by("start_date")
-        programs_qset = (
-            Program.objects.live()
-            .select_related("programpage")
-            .exclude(programpage=None)
-            .order_by("id")
-            .all()
-            .prefetch_related(
-                Prefetch("courses__courseruns", queryset=sorted_courserun_qset)
-            )
-        )
-        courses_qset = (
-            Course.objects.live()
-            .select_related("coursepage")
-            .exclude(coursepage=None)
-            .order_by("id")
-            .all()
-            .prefetch_related(Prefetch("courseruns", queryset=sorted_courserun_qset))
-        )
-        return {"programs": programs_qset, "courses": courses_qset}
-
-    def get_context_data(self, *, object_list=None, **kwargs):
-        base_context_data = super().get_context_data(**kwargs)
-        object_list = base_context_data.pop("object_list")
-        return {
-            **base_context_data,
-            **get_base_context(self.request),
-            "programs": object_list["programs"],
-            "courses": object_list["courses"],
-            "default_image_path": DEFAULT_COURSE_IMG_PATH,
-        }
 
 
 class UserEnrollmentsView(APIView):
