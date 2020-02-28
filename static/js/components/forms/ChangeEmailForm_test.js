b2f4dff2ff8fd89223e5c443ba5ddef7a63bb688
Umar Asghar <umar.asghar@arbisoft.com>
2020-02-28 16:01:57 +0500

    Split account settings page into two forms

--- static/js/components/forms/ChangeEmailForm_test.js
+++ static/js/components/forms/ChangeEmailForm_test.js
@@ -3,20 +3,21 @@ import React from "react"
 import sinon from "sinon"
 import { assert } from "chai"
 import { shallow } from "enzyme"
+import { Formik } from "formik"
 
-import ChangePasswordForm from "./ChangePasswordForm"
+import ChangeEmailForm from "./ChangeEmailForm"
 
 import { findFormikFieldByName } from "../../lib/test_utils"
 
 import { makeUser } from "../../factories/user"
 
-describe("ChangePasswordForm", () => {
+describe("ChangeEmailForm", () => {
   let sandbox, onSubmitStub
 
   const user = makeUser()
 
   const renderForm = () =>
-    shallow(<ChangePasswordForm onSubmit={onSubmitStub} user={user} />)
+    shallow(<ChangeEmailForm onSubmit={onSubmitStub} user={user} />)
 
   beforeEach(() => {
     sandbox = sinon.createSandbox()
@@ -33,9 +34,28 @@ describe("ChangePasswordForm", () => {
     const wrapper = renderForm()
 
     const form = wrapper.find("Formik").dive()
-    assert.ok(findFormikFieldByName(form, "oldPassword").exists())
-    assert.ok(findFormikFieldByName(form, "newPassword").exists())
+    assert.ok(findFormikFieldByName(form, "email").exists())
     assert.ok(findFormikFieldByName(form, "confirmPassword").exists())
     assert.ok(form.find("button[type='submit']").exists())
+  })
+
+  it("confirm password is required to change the email address", async () => {
+    const wrapper = renderForm()
+    try {
+      await wrapper.find(Formik).prop("validate")(
+        {
+          email:           "abc@example.com",
+          confirmPassword: ""
+        },
+        {
+          context: { currentEmail: "abc@example.com" }
+        }
+      )
+    } catch (errors) {
+      assert.equal(
+        errors.confirmPassword,
+        "Confirm Password is a required field"
+      )
+    }
   })
 })
