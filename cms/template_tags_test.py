e8e5002662f794c3b17a63063af0d1aad293bff3
Gavin Sidebottom <gsidebo@mit.edu>
2020-12-08 12:36:22 -0500

    Fixed 404/500 error with missing course thumbnails

--- cms/template_tags_test.py
+++ cms/template_tags_test.py
@@ -1,22 +1,32 @@
 """Tests for custom CMS templatetags"""
+from urllib.parse import urljoin
+
 import pytest
 from wagtail.images.views.serve import generate_signature
 from wagtail_factories import ImageFactory
 
 from cms.templatetags.image_version_url import image_version_url
 
+BASE_URL = "http://localhost"
 
-@pytest.mark.django_db
-def test_image_version_url():
+
+@pytest.mark.parametrize("full_url", [True, False])
+def test_image_version_url(settings, full_url):
     """image_version_url should produce an image URL with the file hash set as the file version in the querystring"""
+    settings.SITE_BASE_URL = BASE_URL
     view_name = "wagtailimages_serve"
     image_id = 1
     file_hash = "abcdefg"
     image_filter = "fill-75x75"
     image = ImageFactory.build(id=image_id, file_hash=file_hash)
     expected_signature = generate_signature(image_id, image_filter, key=None)
-    result_url = image_version_url(image, image_filter, viewname=view_name)
-    assert (
-        result_url
-        == f"/images/{expected_signature}/{image_id}/{image_filter}/?v={file_hash}"
+    result_url = image_version_url(
+        image, image_filter, full_url=full_url, viewname=view_name
     )
+    relative_url = (
+        f"/images/{expected_signature}/{image_id}/{image_filter}/?v={file_hash}"
+    )
+    expected_result_url = (
+        relative_url if full_url is False else urljoin(BASE_URL, relative_url)
+    )
+    assert result_url == expected_result_url
--- cms/templatetags/image_version_url.py
+++ cms/templatetags/image_version_url.py
@@ -1,18 +1,34 @@
 """CMS templatetags"""
 
-from urllib.parse import quote_plus
+from urllib.parse import quote_plus, urljoin
 from django import template
+from django.conf import settings
 from wagtail.images.templatetags.wagtailimages_tags import image_url
 
 register = template.Library()
 
 
 @register.simple_tag()
-def image_version_url(image, filter_spec, viewname="wagtailimages_serve"):
-    """Generates an image URL using Wagtails library and appends a version to the path to enable effective caching"""
+def image_version_url(
+    image, filter_spec, full_url=False, viewname="wagtailimages_serve"
+):
+    """
+    Generates an image URL using Wagtail's library and appends a version to the path to enable effective caching
+
+    Args:
+        image (wagtail.images.models.Image): The image the a URL will be generated for
+        filter_spec (str): A filter specification for the image (see Wagtail docs)
+        full_url (bool): If True, generates the image URL with the full base URL instead of just a relative URL
+        viewname (str): The view name to use for generating the URL
+
+    Returns:
+        str or None: The image URL, or None if the image doesn't exist
+    """
+    if not image:
+        return ""
     generated_image_url = image_url(image, filter_spec, viewname=viewname)
-    return (
-        f"{generated_image_url}?v={quote_plus(image.file_hash)}"
-        if generated_image_url
-        else ""
-    )
+    if not generated_image_url:
+        return ""
+    if full_url:
+        generated_image_url = urljoin(settings.SITE_BASE_URL, generated_image_url)
+    return f"{generated_image_url}?v={quote_plus(image.file_hash)}"
