720e83a7d8ff6f36c55c91098511526ec4546a99
HamzaIbnFarooq <hamza.farooq@arbisoft.com>
2021-01-25 18:49:51 +0500

    adding test for wagtail_image_lazy_load

--- cms/template_tags_test.py
+++ cms/template_tags_test.py
@@ -1,13 +1,18 @@
 """Tests for custom CMS templatetags"""
 from urllib.parse import urljoin
 
+from bs4 import BeautifulSoup
+from django.template import Context, Template
 import pytest
 from wagtail.images.views.serve import generate_signature
+from wagtail.images.tests.utils import get_test_image_file_jpeg
+from wagtail.images.models import Image
 from wagtail_factories import ImageFactory
 
 from cms.templatetags.image_version_url import image_version_url
 
 BASE_URL = "http://localhost"
+pytestmark = [pytest.mark.django_db]
 
 
 @pytest.mark.parametrize("full_url", [True, False])
@@ -30,3 +35,30 @@ def test_image_version_url(settings, full_url):
         relative_url if full_url is False else urljoin(BASE_URL, relative_url)
     )
     assert result_url == expected_result_url
+
+
+def test_wagtail_image_lazy_load():
+    """
+    wagtail_image_lazy_load should produce an image template tag having url of placeholder image in src and
+    url of original image in data-src along with the file hash set as the file version in the querystring
+    """
+    file_hash = "abcdefg"
+    image_filter = "fill-75x75"
+    image_file = get_test_image_file_jpeg(size=(1280, 720))
+    image = Image.objects.create(title="Test", file=image_file, file_hash=file_hash)
+
+    custom_template = Template(
+        "{% load wagtail_image_lazy_load %}"
+        "{% wagtail_image_lazy_load image " + image_filter + " %}"
+    )
+    plugin_template = Template(
+        "{% load lazyimages_tags %}" "{% lazy_image image " + image_filter + " %}"
+    )
+    actuall_img_tag = custom_template.render(Context({"image": image}))
+
+    expected_img_tag = plugin_template.render(Context({"image": image}))
+    expected_img_tag = BeautifulSoup(expected_img_tag, "lxml").img
+    expected_img_tag["src"] = f"{expected_img_tag['src']}?v={file_hash}"
+    expected_img_tag["data-src"] = f"{expected_img_tag['data-src']}?v={file_hash}"
+
+    assert actuall_img_tag == str(expected_img_tag)
