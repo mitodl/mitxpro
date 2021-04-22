b81d10e2ab0d39aaf8a9a528a6fa07b8ee4d5487
HamzaIbnFarooq <hamza.farooq@arbisoft.com>
2021-04-22 15:02:53 +0500

    upgrading dependencies

--- authentication/strategy.py
+++ authentication/strategy.py
@@ -12,14 +12,6 @@ class DjangoRestFrameworkStrategy(DjangoStrategy):
         request = drf_request._request  # pylint: disable=protected-access
         super().__init__(storage, request=request, tpl=tpl)
 
-    def clean_authenticate_args(self, *args, **kwargs):
-        """Cleanup request argument if present, which is passed to authenticate as for Django 1.11"""
-        # this is similar to what DjangoStrategy does, but is specific to DRF's Request type
-        if len(args) > 0 and isinstance(args[0], Request):
-            kwargs["request"], args = args[0], args[1:]
-
-        return super().clean_authenticate_args(*args, **kwargs)
-
     def request_data(self, merge=True):
         """Returns the request data"""
         if not self.drf_request:
