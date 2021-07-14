7fc001cae39d42adff3836f43fbc1f100776907c
asadiqbal08 <asad.iqbal@arbisoft.com>
2021-07-14 17:09:31 +0500

    Version bump node-fetch

--- static/js/lib/test_utils.js
+++ static/js/lib/test_utils.js
@@ -21,9 +21,7 @@ export const assertIsJust = (m: Maybe, val: any) => {
 }
 
 export const findFormikFieldByName = (wrapper: any, name: string) =>
-  wrapper
-    .find("FormikConnect(FieldInner)")
-    .filterWhere(node => node.prop("name") === name)
+  wrapper.find("Field").filterWhere(node => node.prop("name") === name)
 
 export const findFormikErrorByName = (wrapper: any, name: string) =>
   wrapper
