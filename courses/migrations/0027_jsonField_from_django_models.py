bd3c83d1f07b1791d4045830e22ec796e95a7f61
Asad Iqbal <a4asadiqbal@gmail.com>
2022-02-22 19:02:24 +0500

    formatted black

--- courses/migrations/0027_jsonField_from_django_models.py
+++ courses/migrations/0027_jsonField_from_django_models.py
@@ -5,39 +5,37 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('courses', '0026_nullify_expiration_date'),
-    ]
+    dependencies = [("courses", "0026_nullify_expiration_date")]
 
     operations = [
         migrations.AlterField(
-            model_name='courserunenrollmentaudit',
-            name='data_after',
+            model_name="courserunenrollmentaudit",
+            name="data_after",
             field=models.JSONField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='courserunenrollmentaudit',
-            name='data_before',
+            model_name="courserunenrollmentaudit",
+            name="data_before",
             field=models.JSONField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='courserungradeaudit',
-            name='data_after',
+            model_name="courserungradeaudit",
+            name="data_after",
             field=models.JSONField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='courserungradeaudit',
-            name='data_before',
+            model_name="courserungradeaudit",
+            name="data_before",
             field=models.JSONField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='programenrollmentaudit',
-            name='data_after',
+            model_name="programenrollmentaudit",
+            name="data_after",
             field=models.JSONField(blank=True, null=True),
         ),
         migrations.AlterField(
-            model_name='programenrollmentaudit',
-            name='data_before',
+            model_name="programenrollmentaudit",
+            name="data_before",
             field=models.JSONField(blank=True, null=True),
         ),
     ]
