169816a2210f5c0ade6daec0426d9bb70874c40c
Arslan Ashraf <34372316+arslanashraf7@users.noreply.github.com>
2022-11-10 15:53:29 +0500

    feat: add discount_type field and dollars-off support in CouponPaymentVersion and APIs (#2449)
    - Add discount_type field and dollars-off support in CouponPaymentVersion and APIs

--- ecommerce/utils_test.py
+++ ecommerce/utils_test.py
@@ -1,8 +1,9 @@
 """Tests for utility functions for ecommerce"""
 import pytest
 
+from ecommerce.constants import DISCOUNT_TYPE_PERCENT_OFF, DISCOUNT_TYPE_DOLLARS_OFF
 from ecommerce.exceptions import ParseException
-from ecommerce.utils import get_order_id_by_reference_number
+from ecommerce.utils import get_order_id_by_reference_number, validate_amount
 
 
 @pytest.mark.parametrize(
@@ -21,3 +22,27 @@ def test_get_order_id_by_reference_number_parse_error(reference_number, error):
             reference_number=reference_number, prefix="MITXPRO-cyb-prefix"
         )
     assert ex.value.args[0] == error
+
+
+@pytest.mark.parametrize(
+    "discount_type, amount, error",
+    [
+        (
+            DISCOUNT_TYPE_PERCENT_OFF,
+            2,
+            "The amount should be between (0 - 1) when discount type is percent-off.",
+        ),
+        (
+            DISCOUNT_TYPE_DOLLARS_OFF,
+            0,
+            "The amount is invalid, please specify a value greater than 0.",
+        ),
+        (DISCOUNT_TYPE_PERCENT_OFF, 1, None),
+        (DISCOUNT_TYPE_DOLLARS_OFF, 1, None),
+    ],
+)
+def test_validate_amount_with_discount_type(discount_type, amount, error):
+    """
+    Test validate_amount returns proper validation message
+    """
+    assert error == validate_amount(discount_type, amount)
--- static/js/constants.js
+++ static/js/constants.js
@@ -3,6 +3,9 @@
 export const COUPON_TYPE_PROMO = "promo"
 export const COUPON_TYPE_SINGLE_USE = "single-use"
 
+export const DISCOUNT_TYPE_PERCENT_OFF = "percent-off"
+export const DISCOUNT_TYPE_DOLLARS_OFF = "dollars-off"
+
 export const ENROLLABLE_ITEM_ID_SEPARATOR = "+"
 export const PRODUCT_TYPE_PROGRAM = "program"
 export const PRODUCT_TYPE_COURSERUN = "courserun"
