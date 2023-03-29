a86c72cafdf928a9a87bab080f13321c0bee69dd
Asad Ali <asad.ali@arbisoft.com>
2023-03-29 13:39:51 +0500

    tests: add test for homepage topics

--- courses/factories.py
+++ courses/factories.py
@@ -1,8 +1,8 @@
 """Factories for creating course data in tests"""
-import pytz
-import faker
 import factory
-from factory import fuzzy, SubFactory, Trait
+import faker
+import pytz
+from factory import SubFactory, Trait, fuzzy
 from factory.django import DjangoModelFactory
 
 from courses.constants import PROGRAM_TEXT_ID_PREFIX
@@ -10,16 +10,18 @@ from ecommerce.models import Company
 from users.factories import UserFactory
 
 from .models import (
-    Program,
-    ProgramRun,
     Course,
     CourseRun,
-    ProgramEnrollment,
-    CourseRunEnrollment,
     CourseRunCertificate,
-    ProgramCertificate,
+    CourseRunEnrollment,
     CourseRunGrade,
+    CourseTopic,
+    Program,
+    ProgramCertificate,
+    ProgramEnrollment,
+    ProgramRun,
 )
+
 
 FAKE = faker.Factory.create()
 
@@ -175,3 +177,13 @@ class ProgramEnrollmentFactory(DjangoModelFactory):
 
     class Meta:
         model = ProgramEnrollment
+
+
+class CourseTopicFactory(DjangoModelFactory):
+    """Factory for `CourseTopic`"""
+
+    name = fuzzy.FuzzyText(prefix="Topic ")
+    parent = None
+
+    class Meta:
+        model = CourseTopic
