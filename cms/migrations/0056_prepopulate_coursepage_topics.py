1146e609bc830c2d3d70e8a263578401ddcb2c13
Arslan <arslan.ashraf@arbisoft.com>
2023-04-13 18:09:13 +0500

    fix: update the migrations to handle external course topics as well

--- cms/migrations/0056_prepopulate_coursepage_topics.py
+++ cms/migrations/0056_prepopulate_coursepage_topics.py
@@ -10,16 +10,21 @@ def migrate_associate_existing_topics(apps, app_schema):
     ExternalCoursePage = apps.get_model("cms", "ExternalCoursePage")
     CourseTopic = apps.get_model("courses", "CourseTopic")
 
-    all_course_pages = list(CoursePage.objects.all()) + list(
-        ExternalCoursePage.objects.all()
-    )
+    for internal_course_page in CoursePage.objects.all():
+        if internal_course_page.course:
+            topics = CourseTopic.objects.filter(course=internal_course_page.course)
+            for topic in topics:
+                CoursePage.topics.through.objects.create(
+                    coursepage_id=internal_course_page.id, coursetopic_id=topic.id
+                )
 
-    for course_page in all_course_pages:
-        topics = CourseTopic.objects.filter(course=course_page.course)
-        for topic in topics:
-            CoursePage.topics.through.objects.create(
-                coursepage_id=course_page.id, coursetopic_id=topic.id
-            )
+    for external_course_page in ExternalCoursePage.objects.all():
+        if external_course_page.course:
+            topics = CourseTopic.objects.filter(course=external_course_page.course)
+            for topic in topics:
+                ExternalCoursePage.topics.through.objects.create(
+                    externalcoursepage_id=external_course_page.id, coursetopic_id=topic.id
+                )
 
 
 class Migration(migrations.Migration):
