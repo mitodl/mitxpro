3fc0e0991f665a7a338a6aad995638d3e16e0a9d
Muhammad Ghulam Fakhar ud din <muhammad.fakhar@arbisoft.com>
2023-07-06 14:54:08 +0500

    refactor: Code refactored in API and tests completed

--- courses/tasks_test.py
+++ courses/tasks_test.py
@@ -1,1 +1,23 @@
-"""Course tasks tests"""
+"""Tests for Course related tasks"""
+
+import pytest
+
+from courses.tasks import generate_course_certificates, sync_courseruns_data
+
+pytestmark = pytest.mark.django_db
+
+
+def test_generate_course_certificates_task(mocker):
+    """Test generate_course_certificates calls the right api functionality from courses"""
+    generate_course_run_certificates = mocker.patch(
+        "courses.api.generate_course_run_certificates"
+    )
+    generate_course_certificates.delay()
+    generate_course_run_certificates.assert_called_once()
+
+
+def test_sync_courseruns_data(mocker):
+    """Test sync_courseruns_data calls the right api functionality from courses"""
+    sync_course_runs_data_task = mocker.patch("courses.api.sync_course_runs_data")
+    sync_courseruns_data.delay()
+    sync_course_runs_data_task.assert_called_once()
