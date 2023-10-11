8cd9b064e119d40077d522ea657cfa8e1a28582f
Asad Ali <asad.ali@arbisoft.com>
2023-10-11 16:59:26 +0500

    more styles

--- static/js/containers/pages/BlogPage.js
+++ static/js/containers/pages/BlogPage.js
@@ -42,31 +42,41 @@ export class BlogPage extends React.Component {
                   <div className="recent-posts-text">Top Most Recent Posts</div>
                 </div>
                 <div className="top-posts-container">
-                  <div className="featured-post">This is Left Div</div>
+                  {/*<div className="featured-post-container" style={{backgroundImage: "url(/static/images/mit-dome.png)"}}>*/}
+                  <div className="featured-post-container">
+                    {/*<div className="image-container">*/}
+                    {/*  <img src="" alt=""/>*/}
+                    {/*</div>*/}
+                    <div className="post-content">
+                      <span className="post-tag">ONLINE EDUCATION</span>
+                      <div className="featured-post-title">The Competitive Advantages of Online Corporate LearningThe Competitive Advantages of Online Corporate Learning</div>
+                      <div className="featured-post-description">Online corporate learning is one of the most effective ways to deepen your team’s knowledge and expand their abilities regarding everything from the most ground-breaking emerging technologies</div>
+                    </div>
+                  </div>
                   <div className="posts-sidebar">
                     <div className="sidebar-post-card">
-                      <img src="/static/images/mit-dome.png"></img>
+                      <img src="/static/images/mit-dome.png"/>
                       <div className="details">
                         <div className="post-title">What to Read Next: Recommendations from MIT xPRO Faculty</div>
                         <div className="post-description">ONLINE EDUCATION What to Read Next: Recommendations from MIT xPRO Faculty</div>
                       </div>
                     </div>
                     <div className="sidebar-post-card">
-                      <img src="/static/images/mit-dome.png"></img>
+                      <img src="/static/images/mit-dome.png"/>
                       <div className="details">
                         <div className="post-title">What to Read Next: Recommendations from MIT xPRO Faculty</div>
                         <div className="post-description">ONLINE EDUCATION What to Read Next: Recommendations from MIT xPRO Faculty</div>
                       </div>
                     </div>
                     <div className="sidebar-post-card">
-                      <img src="/static/images/mit-dome.png"></img>
+                      <img src="/static/images/mit-dome.png"/>
                       <div className="details">
                         <div className="post-title">What to Read Next: Recommendations from MIT xPRO Faculty</div>
                         <div className="post-description">ONLINE EDUCATION What to Read Next: Recommendations from MIT xPRO Faculty</div>
                       </div>
                     </div>
                     <div className="sidebar-post-card">
-                      <img src="/static/images/mit-dome.png"></img>
+                      <img src="/static/images/mit-dome.png"/>
                       <div className="details">
                         <div className="post-title">What to Read Next: Recommendations from MIT xPRO Faculty</div>
                         <div className="post-description">ONLINE EDUCATION What to Read Next: Recommendations from MIT xPRO Faculty</div>
@@ -127,34 +137,106 @@ export class BlogPage extends React.Component {
                 </div>
                 <div className="posts-list">
                   <div className="post">
-                    <img src="/static/images/mit-dome.png" alt="Preview image" />
+                    <div className="card-top">
+                      <img src="/static/images/mit-dome.png" alt="Preview image" />
+                      <span className="post-tag">ONLINE EDUCATION</span>
+                    </div>
                     <a className="title" href="">What to Read Next: Recommendations from MIT xPRO Faculty</a>
                     <p className="description">Wondering what to read next? Ready to stimulate your brain with topics ranging from systems engineering to artificial intelligence? You’ve come to the right place.</p>
+                    <div className="card-bottom">
+                      <div className="author-and-duration">
+                        <div className="author">BY: MIT XPRO | JUNE 7TH, 2023</div>
+                        <div className="duration">5 MINUTE READ</div>
+                      </div>
+                      <div className="read-more">
+                        <a href="">READ MORE</a>
+                      </div>
+                    </div>
                   </div>
                   <div className="post">
-                    <img src="/static/images/mit-dome.png" alt="Preview image" />
+                    <div className="card-top">
+                      <img src="/static/images/mit-dome.png" alt="Preview image" />
+                      <span className="post-tag">ONLINE EDUCATION</span>
+                    </div>
                     <a className="title" href="">What to Read Next: Recommendations from MIT xPRO Faculty</a>
                     <p className="description">Wondering what to read next? Ready to stimulate your brain with topics ranging from systems engineering to artificial intelligence? You’ve come to the right place.</p>
+                    <div className="card-bottom">
+                      <div className="author-and-duration">
+                        <div className="author">BY: MIT XPRO | JUNE 7TH, 2023</div>
+                        <div className="duration">5 MINUTE READ</div>
+                      </div>
+                      <div className="read-more">
+                        <a href="">READ MORE</a>
+                      </div>
+                    </div>
                   </div>
                   <div className="post">
-                    <img src="/static/images/mit-dome.png" alt="Preview image" />
+                    <div className="card-top">
+                      <img src="/static/images/mit-dome.png" alt="Preview image" />
+                      <span className="post-tag">ONLINE EDUCATION</span>
+                    </div>
                     <a className="title" href="">What to Read Next: Recommendations from MIT xPRO Faculty</a>
                     <p className="description">Wondering what to read next? Ready to stimulate your brain with topics ranging from systems engineering to artificial intelligence? You’ve come to the right place.</p>
+                    <div className="card-bottom">
+                      <div className="author-and-duration">
+                        <div className="author">BY: MIT XPRO | JUNE 7TH, 2023</div>
+                        <div className="duration">5 MINUTE READ</div>
+                      </div>
+                      <div className="read-more">
+                        <a href="">READ MORE</a>
+                      </div>
+                    </div>
                   </div>
                   <div className="post">
-                    <img src="/static/images/mit-dome.png" alt="Preview image" />
+                    <div className="card-top">
+                      <img src="/static/images/mit-dome.png" alt="Preview image" />
+                      <span className="post-tag">ONLINE EDUCATION</span>
+                    </div>
                     <a className="title" href="">What to Read Next: Recommendations from MIT xPRO Faculty</a>
                     <p className="description">Wondering what to read next? Ready to stimulate your brain with topics ranging from systems engineering to artificial intelligence? You’ve come to the right place.</p>
+                    <div className="card-bottom">
+                      <div className="author-and-duration">
+                        <div className="author">BY: MIT XPRO | JUNE 7TH, 2023</div>
+                        <div className="duration">5 MINUTE READ</div>
+                      </div>
+                      <div className="read-more">
+                        <a href="">READ MORE</a>
+                      </div>
+                    </div>
                   </div>
                   <div className="post">
-                    <img src="/static/images/mit-dome.png" alt="Preview image" />
+                    <div className="card-top">
+                      <img src="/static/images/mit-dome.png" alt="Preview image" />
+                      <span className="post-tag">ONLINE EDUCATION</span>
+                    </div>
                     <a className="title" href="">What to Read Next: Recommendations from MIT xPRO Faculty</a>
                     <p className="description">Wondering what to read next? Ready to stimulate your brain with topics ranging from systems engineering to artificial intelligence? You’ve come to the right place.</p>
+                    <div className="card-bottom">
+                      <div className="author-and-duration">
+                        <div className="author">BY: MIT XPRO | JUNE 7TH, 2023</div>
+                        <div className="duration">5 MINUTE READ</div>
+                      </div>
+                      <div className="read-more">
+                        <a href="">READ MORE</a>
+                      </div>
+                    </div>
                   </div>
                   <div className="post">
-                    <img src="/static/images/mit-dome.png" alt="Preview image" />
+                    <div className="card-top">
+                      <img src="/static/images/mit-dome.png" alt="Preview image" />
+                      <span className="post-tag">ONLINE EDUCATION</span>
+                    </div>
                     <a className="title" href="">What to Read Next: Recommendations from MIT xPRO Faculty</a>
                     <p className="description">Wondering what to read next? Ready to stimulate your brain with topics ranging from systems engineering to artificial intelligence? You’ve come to the right place.</p>
+                    <div className="card-bottom">
+                      <div className="author-and-duration">
+                        <div className="author">BY: MIT XPRO | JUNE 7TH, 2023</div>
+                        <div className="duration">5 MINUTE READ</div>
+                      </div>
+                      <div className="read-more">
+                        <a href="">READ MORE</a>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
