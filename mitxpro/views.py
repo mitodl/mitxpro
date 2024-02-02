e8b7827f7d423a4c7f37b95e1bbac9ea825ab080
Asad Ali <asad.ali@arbisoft.com>
2024-02-02 13:33:47 +0500

    feat: add prod zd-site-verification and hard code it (#2883)

--- mitxpro/views.py
+++ mitxpro/views.py
@@ -26,11 +26,6 @@ def get_base_context(request):
         context[
             "domain_verification_tag"
         ] = settings.GOOGLE_DOMAIN_VERIFICATION_TAG_VALUE
-
-    if settings.ZENDESK_DOMAIN_VERIFICATION_TAG_VALUE:
-        context[
-            "zendesk_domain_verification_tag"
-        ] = settings.ZENDESK_DOMAIN_VERIFICATION_TAG_VALUE
     return context
 
 
