0551bcb9900ad8240f13fe7d81d49d74becee092
Asad Ali <asad.ali@arbisoft.com>
2024-02-02 11:45:41 +0500

    feat: add prod zd-site-verification and hard code it

--- mitxpro/settings.py
+++ mitxpro/settings.py
@@ -1338,11 +1338,6 @@ GOOGLE_DOMAIN_VERIFICATION_TAG_VALUE = get_string(
     default=None,
     description="The value of the meta tag used by Google to verify the owner of a domain (used for enabling push notifications)",
 )
-ZENDESK_DOMAIN_VERIFICATION_TAG_VALUE = get_string(
-    name="ZENDESK_DOMAIN_VERIFICATION_TAG_VALUE",
-    default=None,
-    description="The value of the meta tag used by Zendesk to verify the owner of a domain",
-)
 SHEETS_ADMIN_EMAILS = get_delimited_list(
     name="SHEETS_ADMIN_EMAILS",
     default=[],
