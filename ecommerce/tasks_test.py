9ab1adf8baf2e250abf370d4913fc0c07bed2e65
Muhammad Anas <muhammad.anas@arbisoft.com>
2024-06-21 10:06:57 +0000

    refactor: test

--- ecommerce/tasks_test.py
+++ ecommerce/tasks_test.py
@@ -7,13 +7,14 @@ from ecommerce.models import Basket, BasketItem, CouponSelection, CourseRunSelec
 
 
 def test_delete_expired_baskets(mocker, user, basket_and_coupons):
+    basket_and_coupons.basket.user = user
+    basket_and_coupons.basket.save()
+    
     now_in_utc = mocker.patch("ecommerce.tasks.now_in_utc")
     now_in_utc.return_value = datetime.datetime.now(
         tz=datetime.timezone.utc
-    ) + datetime.timedelta(days=settings.BASKET_EXPIRY_DAYS + 1)
+    ) + datetime.timedelta(days=settings.BASKET_EXPIRY_DAYS)
 
-    basket_and_coupons.basket.user = user
-    basket_and_coupons.basket.save()
     assert Basket.objects.filter(user=user).count() == 1
     assert BasketItem.objects.filter(basket__user=user).count() > 0
     assert CourseRunSelection.objects.filter(basket__user=user).count() > 0
