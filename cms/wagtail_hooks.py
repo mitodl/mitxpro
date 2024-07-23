208ea36025d7e364343e2e57becedcd65edd9f58
Asad Ali <asad.ali@arbisoft.com>
2024-07-23 14:24:49 +0500

    fix: use after page publish hook to create product and versions to avoid creating them on draft

--- cms/wagtail_hooks.py
+++ cms/wagtail_hooks.py
@@ -1,7 +1,11 @@
 """Custom hooks to configure wagtail behavior"""
 
+from django.contrib.contenttypes.models import ContentType
 from wagtail import hooks
 from wagtail.admin.api.views import PagesAdminAPIViewSet
+
+from courses.models import CourseRun, Program
+from ecommerce.models import Product, ProductVersion
 
 
 @hooks.register("construct_explorer_page_queryset")
@@ -26,3 +30,46 @@ class OrderedPagesAPIEndpoint(PagesAdminAPIViewSet):
 def configure_admin_api_default_order(router):
     """Swap admin pages API for our own flavor that orders results by title"""
     router.register_endpoint("pages", OrderedPagesAPIEndpoint)
+
+
+@hooks.register("after_publish_page")
+def create_product_and_product_versions(request, page):
+    """
+    Creates Product and Product Version for the courseware pages on page publish.
+    """
+    if not (
+        page.is_internal_or_external_course_page
+        or page.is_internal_or_external_program_page
+    ):
+        return
+
+    form_class = page.specific_class.get_edit_handler().get_form_class()
+    form = form_class(request.POST, instance=page)
+    if not form.is_valid():
+        return
+
+    course_run_id = form.cleaned_data["course_run"]
+    price = form.cleaned_data["price"]
+
+    if page.is_internal_or_external_course_page and course_run_id and price:
+        course_run = CourseRun.objects.get(id=course_run_id)
+        if course_run.current_price != price:
+            product, _ = Product.objects.get_or_create(
+                content_type=ContentType.objects.get_for_model(CourseRun),
+                object_id=course_run.id,
+            )
+            ProductVersion.objects.create(
+                product=product, price=price, description=course_run.text_id
+            )
+
+    elif (
+        page.is_internal_or_external_program_page
+        and price != page.program.current_price
+    ):
+        product, _ = Product.objects.get_or_create(
+            content_type=ContentType.objects.get_for_model(Program),
+            object_id=page.program.id,
+        )
+        ProductVersion.objects.create(
+            product=product, price=price, description=page.program.text_id
+        )
