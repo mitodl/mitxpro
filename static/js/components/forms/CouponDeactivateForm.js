39c770dfacfa30156e432d94c8e4e9582937ae94
Muhammad Anas <muhammad.anas@arbisoft.com>
2024-10-10 08:16:53 +0000

    fix: included example in the note

--- static/js/components/forms/CouponDeactivateForm.js
+++ static/js/components/forms/CouponDeactivateForm.js
@@ -30,10 +30,12 @@ export const CouponDeactivateForm = ({
         <div>
           <div className="block text-area-div">
             <label htmlFor="coupons">
-              Coupon Codes/Names (each coupon at a separate line)*
+              Coupon Codes/Names (each code/name at a separate line)*
               <p className="small-text warning">
-                Note: Adding a coupon name will deactivate all coupon codes
-                associated with that name.
+                Note: Adding coupon code like <strong>dd5475f4c1c</strong> will
+                deactivate the specific coupon. Adding a coupon name like{" "}
+                <strong>MIT_DIGITALLEARNING_25</strong> will deactivate all the
+                coupon codes i.e. <strong>dd5475f4c1c ab064b8215f2 ...</strong>
               </p>
               <Field name="coupons" component="textarea" rows="4" />
             </label>
