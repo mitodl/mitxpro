9df5e92fc937d8b9f04e336b75c1886390f9a370
Arslan <arslan.ashraf@arbisoft.com>
2024-10-31 16:17:09 +0500

    feat: add management command for topics assignment to courses

---
+++ courses/management/commands/manage_topics.py
@@ -0,0 +1,245 @@
+"""Management command to bulk load topics from a CSV into the system
+
+Arguments:
+* --file <filename> - Path to the CSV file containing the topics OR the file containing the topics and courses titles (But only one at a time)
+* --create-topics <filename> - Flag to decide that we should create topics from topics sheet (Format mentioned below)
+* --assign-topics <filename> - Flag to decide that we should only populate topics from course title & topics sheet (Format mentioned below
+
+## Conditions for a valid topics file (Used for * --create-topics)
+--file must be a valid CSV file. The command assumes that the file would have a header row containing all the topics/subtopics.
+The sample file format can be seen below:
+
+Parent Topic       | Parent Topic         | Parent Topic
+subtopic           | subtopic             | subtopic
+subtopic           | subtopic             | subtopic
+
+
+## Conditions for a valid topics & titles file (Used for * --assign-topics)
+--file must be a valid CSV file. The command assumes that the file would have a header row containing product_platform, product_name, High-Level Topic 1, Sub-topic 1, High-Level Topic 2, Sub-topic 2
+The sample file format can be seen below:
+
+product_platform   | product_name      | High-Level Topic 1        | Sub-topic 1       | High-Level Topic 2        | Sub-topic 2
+xPRO               | Course title      | Parent Topic 1            | Subtopic 1        | Parent Topic 2            | Subtopic 2
+Global Alumni      | Course title      | Parent Topic 1            | Subtopic 1        | Parent Topic 2            | Subtopic 2
+Emeritus           | Course title      | Parent Topic 1            | Subtopic 1        | Parent Topic 2            | Subtopic 2
+
+"""
+
+import csv
+from pathlib import Path
+
+from django.core.management import BaseCommand, CommandError
+
+from cms.models import CoursePage, ExternalCoursePage
+from courses.constants import DEFAULT_PLATFORM_NAME
+from courses.models import CourseTopic
+
+PLATFORM_COLUMN_NAME = "product_platform"
+COURSE_TITLE_COLUMN_NAME = "product_name"
+PARENT_TOPIC_1_COLUMN_NAME = "High-Level Topic 1"
+SUB_TOPIC_1_COLUMN_NAME = "Sub-topic 1"
+PARENT_TOPIC_2_COLUMN_NAME = "High-Level Topic 2"
+SUB_TOPIC_2_COLUMN_NAME = "Sub-topic 2"
+
+REQUIRED_COLUMNS = [
+    PLATFORM_COLUMN_NAME,
+    COURSE_TITLE_COLUMN_NAME,
+    PARENT_TOPIC_1_COLUMN_NAME,
+    SUB_TOPIC_1_COLUMN_NAME,
+    PARENT_TOPIC_2_COLUMN_NAME,
+    SUB_TOPIC_2_COLUMN_NAME,
+]
+
+
+def create_topic(name=None, parent=None):
+    """Helper method to create topics based on topic/subtopic relationship"""
+    name = name.strip()
+    if not parent:
+        return CourseTopic.objects.get_or_create(name=name)
+    parent = parent.strip()
+    parent_topic = CourseTopic.objects.get(name=parent, parent__isnull=True)
+    return CourseTopic.objects.get_or_create(name=name, parent=parent_topic)
+
+
+def get_topic(name=None, parent=None):
+    """Helper method to retrieve topics based on topic/subtopic relationship"""
+    if name is None:
+        return None
+    name = name.strip()
+    parent = parent.strip() if parent else None
+    try:
+        return CourseTopic.objects.get(name__iexact=name, parent__name__iexact=parent)
+    except CourseTopic.DoesNotExist:
+        return None
+
+
+def perform_create_topics(file_path):
+    """Read the data from the CSV file and create topics"""
+    stats = {
+        "new_topics": [],
+        "skipped_topics": [],
+        "new_subtopics": [],
+        "skipped_subtopics": [],
+    }
+
+    with open(file_path) as import_raw:  # noqa: PTH123
+        data_dict = csv.DictReader(import_raw)
+        # Top level topic | Parent topics
+        # data_dict.fieldnames are all the column names in the header of the CSV file.
+        # We will create top level topics assuming each column name represents a top level topic.
+        topics = data_dict.fieldnames
+
+        for topic in topics:
+            topic_obj, created = create_topic(topic)
+            stats["new_topics"].append(topic_obj.name) if created else stats[
+                "skipped_topics"
+            ].append(topic_obj.name)
+
+        # Each row after the header contains the subtopics respectively according to their parent in each column
+        for row in data_dict:
+            for topic, subtopic in row.items():
+                if subtopic:
+                    subtopic_obj, created = create_topic(subtopic, topic)
+                    stats["new_subtopics"].append(
+                        subtopic_obj.name
+                    ) if created else stats["skipped_subtopics"].append(
+                        subtopic_obj.name
+                    )
+
+        return stats
+
+
+def perform_assign_topics(file_path):
+    """Read the data from the CSV file and associate the topics to courses"""
+    stats = []
+    errors = []
+
+    with open(file_path) as import_raw:  # noqa: PTH123
+        data_dict = csv.DictReader(import_raw)
+        # data_dict.fieldnames are all the column names in the header of the topics association CSV file.
+        # Also, We want to scope the things while association so we will strictly need the right data. To make sure we have the
+        # right data we will cross check that the sheet contains the required columns
+        columns = data_dict.fieldnames
+        if not all(required_column in columns for required_column in REQUIRED_COLUMNS):
+            errors.append(
+                "The file data is invalid. Please check file has all the columns."
+            )
+        else:
+            for row in data_dict:
+                platform_name = row.get(PLATFORM_COLUMN_NAME)
+                course_title = row.get(COURSE_TITLE_COLUMN_NAME)
+                parent_topic1 = row.get(PARENT_TOPIC_1_COLUMN_NAME)
+                sub_topic1 = row.get(SUB_TOPIC_1_COLUMN_NAME)
+                parent_topic2 = row.get(PARENT_TOPIC_2_COLUMN_NAME)
+                sub_topic2 = row.get(SUB_TOPIC_2_COLUMN_NAME)
+                course_page = None
+                try:
+                    if platform_name.lower() == DEFAULT_PLATFORM_NAME.lower():
+                        # This is an internal course
+                        course_page = CoursePage.objects.get(
+                            title__iexact=course_title,
+                            course__platform__name=platform_name,
+                        )
+                    else:
+                        course_page = ExternalCoursePage.objects.get(
+                            title__iexact=course_title,
+                            course__platform__name=platform_name,
+                        )
+                except (CoursePage.DoesNotExist, ExternalCoursePage.DoesNotExist):
+                    errors.append(f"{course_title}: Does not exist.")
+
+                if course_page:
+                    parent_topic1 = get_topic(name=parent_topic1)
+                    sub_topic1 = (
+                        get_topic(name=sub_topic1, parent=parent_topic1.name)
+                        if parent_topic1
+                        else None
+                    )
+                    parent_topic2 = get_topic(name=parent_topic2)
+                    sub_topic2 = (
+                        get_topic(name=sub_topic2, parent=parent_topic2.name)
+                        if parent_topic2
+                        else None
+                    )
+
+                    course_page.topics.add(parent_topic1) if parent_topic1 else None
+                    course_page.topics.add(sub_topic1) if sub_topic1 else None
+
+                    # If sub_topic 2 is blank we only assign High Level topic 1 and Subtopic 1 (See: https://github.com/mitodl/hq/issues/5841#issuecomment-2447413927)
+                    if sub_topic2:
+                        course_page.topics.add(parent_topic2) if parent_topic2 else None
+                        course_page.topics.add(sub_topic2)
+
+                    course_page.save()
+
+                    stats.append(f"{course_title}: Topics association success!")
+
+    return errors, stats
+
+
+class Command(BaseCommand):
+    """The command reads a list of topics/subtopics from a CSV file and loads them into system"""
+
+    help = "Bulk loads the topics from a CSV file"
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--file",
+            type=str,
+            help="Path to the CSV file containing topics",
+            required=True,
+            dest="topics_file",
+        )
+        parser.add_argument(
+            "--create-topics",
+            action="store_true",
+            help="Perform the topics creation operation",
+        )
+        parser.add_argument(
+            "--assign-topics",
+            action="store_true",
+            help="Perform the topics assignment operation",
+        )
+
+    def handle(self, *args, **options):  # noqa: ARG002
+        """Handle command execution"""
+
+        file_path = options["topics_file"].strip()
+        arg_create_topics = options["create_topics"]
+        arg_assign_topics = options["assign_topics"]
+
+        if not any([arg_create_topics, arg_assign_topics]):
+            raise CommandError(
+                "Please select the operation to perform. Options are --create-topics or --assign-topics"  # noqa: EM101
+            )
+        if arg_create_topics and arg_assign_topics:
+            raise CommandError("Only one operation is allowed.")  # noqa: EM101
+
+        if not file_path or not file_path.endswith(".csv"):
+            raise CommandError("The command can handle only CSV files")  # noqa: EM101
+
+        if not Path(file_path).exists():
+            raise CommandError("Invalid file path")  # noqa: EM101
+        if arg_create_topics:
+            # Perform the create topics operation
+            stats = perform_create_topics(file_path=file_path)
+            self.stdout.write(
+                self.style.SUCCESS(f"Topics created: {stats['new_topics']}\n")
+            )
+            self.stdout.write(
+                self.style.SUCCESS(f"Topics skipped: {stats['skipped_topics']}\n")
+            )
+            self.stdout.write(
+                self.style.SUCCESS(f"Subtopics created: {stats['new_subtopics']}\n")
+            )
+            self.stdout.write(
+                self.style.SUCCESS(f"Subtopics skipped: {stats['skipped_subtopics']}\n")
+            )
+
+        else:
+            errors, stats = perform_assign_topics(file_path=file_path)
+            for stat in stats:
+                self.stdout.write(self.style.SUCCESS(f"{stat}\n"))
+
+            for error in errors:
+                self.stdout.write(self.style.ERROR(f"{error}\n"))
