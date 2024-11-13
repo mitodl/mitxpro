4eca9506a4223625c8ee89d2c4546f5a9b90b0c7
Asad Ali <asad.ali@arbisoft.com>
2024-11-13 19:02:20 +0500

    refactor

--- scripts/test/python_tests.sh
+++ scripts/test/python_tests.sh
@@ -14,6 +14,7 @@ function run_test {
 
 run_test ./scripts/test/detect_missing_migrations.sh
 run_test ./scripts/test/no_auto_migrations.sh
+run_test pytest -n logical
 
 # shellcheck disable=SC2154
 exit $status
