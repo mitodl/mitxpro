9bde93947b3f9d08bdfa0f76cfc422a6503dbf89
Muhammad Arslan <arslan.abdulrauf@arbisoft.com>
2024-12-20 14:45:54 +0500

    feat: How you will learn and B2B section added for external courses (#3318)
    * feat: How you will learn and B2B section added for external courses
    
    * section creation moved from model save to wagtail hook
    test case added for new hook "after_create_page"
    
    * feat: Management command added to backfil sections in external course pages
    
    * feat: Attach predefined static pages to ExternalCoursePage instances
    Updated logic to copy and attach static pages (e.g., Learning Techniques, For Teams) to newly created ExternalCoursePage instances.
    
    * test: Add test cases for CommonExternalCoursePages and its creation
    
    * feat: Uniqueness added for CommonExternalCourse child pages
    
    * doc: doc string added for test
    
    * refactor: Check existing pages in utils
    
    * feat: Common child index page created
    
    * feat: Remove ununsed images
    
    * fix: Auto creation of draft common pages fixed
    docs: extensive doc strings added
    
    * feat: Restructure architecture for CommonComponentIndexPage
    test: test cases added for cms/utils
    
    * fix: Revert other models changes
    
    * fix: Duplicate title page creation
    refactor: Improve Test cases by using parameters
    
    * fix: fix slug issue with same title

---
+++ cms/utils.py
@@ -0,0 +1,84 @@
+"""
+This module provides utility functions for creating and adding specific sections to a given page in a Django CMS application.
+"""
+
+from django.db import models
+
+from cms.models import (
+    ForTeamsCommonPage,
+    ForTeamsPage,
+    LearningTechniquesCommonPage,
+    LearningTechniquesPage,
+)
+
+
+def create_and_add_how_you_will_learn_section(page, platform=None):
+    """
+    Creates and adds a LearningTechniquesPage section to the given page.
+
+    Args:
+        page: The parent page where the LearningTechniquesPage section will be added.
+        platform (str, optional): The name of the platform to filter the learning
+            techniques content. Defaults to None.
+
+    Returns:
+        None
+    """
+    icongrid_page = page.get_child_page_of_type_including_draft(LearningTechniquesPage)
+    if icongrid_page:
+        return
+
+    learning_tech_page = (
+        LearningTechniquesCommonPage.objects.filter(
+            models.Q(platform__name__iexact=platform)
+            | models.Q(platform__name__isnull=True),
+            live=True,
+        )
+        .order_by("platform__name")
+        .first()
+    )
+    if not learning_tech_page:
+        return
+
+    tech_page = LearningTechniquesPage(
+        title=learning_tech_page.title,
+        technique_items=learning_tech_page.technique_items,
+    )
+    page.add_child(instance=tech_page)
+
+
+def create_and_add_b2b_section(page, platform=None):
+    """
+    Creates and adds a "For Teams" (B2B) section to the given page.
+
+    Args:
+        page: The parent page where the "For Teams" section will be added.
+        platform (str, optional): The name of the platform to filter the B2B content. Defaults to None.
+
+    Returns:
+        None
+    """
+    b2b_page = page.get_child_page_of_type_including_draft(ForTeamsPage)
+    if b2b_page:
+        return
+
+    b2b_page = (
+        ForTeamsCommonPage.objects.filter(
+            models.Q(platform__name__iexact=platform)
+            | models.Q(platform__name__isnull=True),
+            live=True,
+        )
+        .order_by("platform__name")
+        .first()
+    )
+    if not b2b_page:
+        return
+
+    for_teams_page = ForTeamsPage(
+        title=b2b_page.title,
+        content=b2b_page.content,
+        action_title=b2b_page.action_title,
+        action_url=b2b_page.action_url,
+        image=b2b_page.image,
+    )
+    page.add_child(instance=for_teams_page)
