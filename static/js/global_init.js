0978681f2a8461f35aa7514aec81b1740b56ad6a
Muhammad Anas <muhammad.anas@arbisoft.com>
2024-12-31 10:28:23 +0000

    temp: test

--- static/js/global_init.js
+++ static/js/global_init.js
@@ -1,9 +1,9 @@
 // Define globals we would usually get from Django
 import ReactDOM from "react-dom";
-import { configure } from "enzyme";
+import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
-configure({ adapter: new Adapter() });
+Enzyme.configure({ adapter: new Adapter() });
 
 const _createSettings = () => ({});
 
