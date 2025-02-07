30646b5d962eab3434331d778058fd8d0d04ac70
Muhammad Anas <muhammad.anas@arbisoft.com>
2025-02-07 19:04:20 +0500

    config: changed default backend to ol-oauth

--- mitxpro/settings.py
+++ mitxpro/settings.py
@@ -1073,13 +1073,13 @@ MITOL_AUTHENTICATION_REPLY_TO_EMAIL = MITXPRO_REPLY_TO_ADDRESS
 
 OPENEDX_OAUTH_PROVIDER = get_string(
     name="OPENEDX_OAUTH_PROVIDER",
-    default="mitxpro-oauth2",
+    default="ol-oauth2",
     description="Social auth oauth provider backend name",
 )
 
 OPENEDX_SOCIAL_LOGIN_PATH = get_string(
     name="OPENEDX_SOCIAL_LOGIN_PATH",
-    default="/auth/login/mitxpro-oauth2/?auth_entry=login",
+    default="/auth/login/ol-oauth2/?auth_entry=login",
     description="Open edX social auth login url",
 )
 
