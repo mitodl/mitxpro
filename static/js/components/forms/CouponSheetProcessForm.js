94abcb62709cd7427f38b5b9b4064ec0f4e883bb
Muhammad Anas <muhammad.anas@arbisoft.com>
2025-02-19 17:05:32 +0000

    fix: issues

--- static/js/components/forms/CouponSheetProcessForm.js
+++ static/js/components/forms/CouponSheetProcessForm.js
@@ -1,3 +1,4 @@
+// @flow
 import React from "react";
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import * as yup from "yup";
@@ -11,72 +12,91 @@ type CouponSheetProcessFormProps = {
 };
 
 const couponValidations = yup.object().shape({
-  sheet_identifier_value: yup
-    .string()
-    .when("sheet_identifier_type", {
-      is: SHEET_IDENTIFIER_ID,
-      then: () => yup
+  sheet_identifier_value: yup.string().when("sheet_identifier_type", {
+    is: SHEET_IDENTIFIER_ID,
+    then: () =>
+      yup
         .string()
         .required("Sheet ID is required")
-        .matches(/^[\w-]+$/, "Only letters, numbers, underscores, and hyphens allowed (no spaces)"),
-      otherwise: () => yup
+        .matches(
+          /^[\w-]+$/,
+          "Only letters, numbers, underscores, and hyphens allowed (no spaces)",
+        ),
+    otherwise: () =>
+      yup
         .string()
         .required("Sheet Title is required")
-        .matches(/^[\w\s-]+$/, "Only letters, numbers, spaces, underscores, and hyphens allowed"),
-    }),
+        .matches(
+          /^[\w\s-]+$/,
+          "Only letters, numbers, spaces, underscores, and hyphens allowed",
+        ),
+  }),
 });
 
-export const CouponSheetProcessForm = ({ onSubmit }: CouponSheetProcessFormProps) => {
-  return (
-    <Formik
-      onSubmit={onSubmit}
-      validationSchema={couponValidations}
-      initialValues={{
-        sheet_identifier_value: "",
-        sheet_identifier_type: SHEET_IDENTIFIER_ID,
-      }}
-      render={({ isSubmitting, setFieldValue, values }) => (
-        <Form className="coupon-form">
-          <div>
-            <div className="flex">
-              <div>
-                <Field
-                  type="radio"
-                  name="sheet_identifier_type"
-                  value={SHEET_IDENTIFIER_ID}
-                  onClick={() => setFieldValue("sheet_identifier_type", SHEET_IDENTIFIER_ID)}
-                  checked={values.sheet_identifier_type===SHEET_IDENTIFIER_ID}
-                />
-                <label htmlFor="sheet_identifier_type">Use Sheet ID</label>
-              </div>
-              <div>
-                <Field
-                  type="radio"
-                  name="sheet_identifier_type"
-                  value={SHEET_IDENTIFIER_TITLE}
-                  onClick={() => setFieldValue("sheet_identifier_type", SHEET_IDENTIFIER_TITLE)}
-                  checked={values.sheet_identifier_type===SHEET_IDENTIFIER_TITLE}
-                />
-                <label htmlFor="sheet_identifier_type">Use Sheet Title</label>
-                </div>
+export const CouponSheetProcessForm = ({
+  onSubmit,
+}: CouponSheetProcessFormProps) => (
+  <Formik
+    onSubmit={onSubmit}
+    validationSchema={couponValidations}
+    initialValues={{
+      sheet_identifier_value: "",
+      sheet_identifier_type: SHEET_IDENTIFIER_ID,
+    }}
+    render={({ isSubmitting, setFieldValue, values }) => (
+      <Form className="coupon-form">
+        <div>
+          <div className="flex">
+            <div>
+              <Field
+                type="radio"
+                name="sheet_identifier_type"
+                value={SHEET_IDENTIFIER_ID}
+                onClick={() =>
+                  setFieldValue("sheet_identifier_type", SHEET_IDENTIFIER_ID)
+                }
+                checked={values.sheet_identifier_type === SHEET_IDENTIFIER_ID}
+              />
+              <label htmlFor="sheet_identifier_type">Use Sheet ID</label>
             </div>
-
-            <div className="block text-area-div">
-              <label htmlFor="sheet_identifier_value">
-                {values.sheet_identifier_type===SHEET_IDENTIFIER_ID ? "Sheet ID*" : "Sheet Title*"}
-                <Field name="sheet_identifier_value" component="textarea" rows="2" cols="40" />
-              </label>
-              <ErrorMessage name="sheet_identifier_value" component={FormError} />
+            <div>
+              <Field
+                type="radio"
+                name="sheet_identifier_type"
+                value={SHEET_IDENTIFIER_TITLE}
+                onClick={() =>
+                  setFieldValue("sheet_identifier_type", SHEET_IDENTIFIER_TITLE)
+                }
+                checked={
+                  values.sheet_identifier_type === SHEET_IDENTIFIER_TITLE
+                }
+              />
+              <label htmlFor="sheet_identifier_type">Use Sheet Title</label>
             </div>
           </div>
 
-          <div>
-            <button type="submit" disabled={isSubmitting}>
-              Process Coupon Sheet
-            </button>
+          <div className="block text-area-div">
+            <label htmlFor="sheet_identifier_value">
+              {values.sheet_identifier_type === SHEET_IDENTIFIER_ID
+                ? "Sheet ID*"
+                : "Sheet Title*"}
+              <Field
+                name="sheet_identifier_value"
+                component="textarea"
+                rows="2"
+                cols="40"
+              />
+            </label>
+            <ErrorMessage name="sheet_identifier_value" component={FormError} />
           </div>
-        </Form>
-      )}
-    />
-  );
-};
+        </div>
+
+        <div>
+          <button type="submit" disabled={isSubmitting}>
+            Process Coupon Sheet
+          </button>
+        </div>
+      </Form>
+    )}
+  />
+);
