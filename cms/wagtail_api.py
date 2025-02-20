c56aa6c3231f757d75254cddefff91ca4119a34c
asajjad2 <areebsajjad21@gmail.com>
2025-02-20 12:18:51 +0500

    feat: wagtail API permissions updated

--- cms/wagtail_api.py
+++ cms/wagtail_api.py
@@ -1,4 +1,5 @@
 from django.db.models import F
+from rest_framework.permissions import IsAdminUser
 from wagtail.api.v2.router import WagtailAPIRouter
 from wagtail.api.v2.views import PagesAPIViewSet
 from wagtail.images.api.v2.views import ImagesAPIViewSet
@@ -7,17 +8,19 @@ from wagtail.documents.api.v2.views import DocumentsAPIViewSet
 from .wagtail_api_filters import ReadableIDFilter
 
 
-class CustomPagesAPIViewSet(PagesAPIViewSet):
+class AdminOnlyViewSetMixin:
+    permission_classes = (IsAdminUser,)
+
+
+class CustomPagesAPIViewSet(AdminOnlyViewSetMixin, PagesAPIViewSet):
+    filter_backends = [ReadableIDFilter] + PagesAPIViewSet.filter_backends
     meta_fields = PagesAPIViewSet.meta_fields + ["live", "last_published_at"]
     listing_default_fields = PagesAPIViewSet.listing_default_fields + [
         "live",
         "last_published_at",
     ]
-    filter_backends = [ReadableIDFilter] + PagesAPIViewSet.filter_backends
     known_query_parameters = PagesAPIViewSet.known_query_parameters.union(
-        [
-            "readable_id",
-        ]
+        ["readable_id"]
     )
 
     def get_queryset(self):
@@ -28,7 +31,15 @@ class CustomPagesAPIViewSet(PagesAPIViewSet):
         return queryset
 
 
+class CustomImagesAPIViewSet(AdminOnlyViewSetMixin, ImagesAPIViewSet):
+    pass
+
+
+class CustomDocumentsAPIViewSet(AdminOnlyViewSetMixin, DocumentsAPIViewSet):
+    pass
+
+
 api_router = WagtailAPIRouter("wagtailapi")
 api_router.register_endpoint("pages", CustomPagesAPIViewSet)
-api_router.register_endpoint("images", ImagesAPIViewSet)
-api_router.register_endpoint("documents", DocumentsAPIViewSet)
+api_router.register_endpoint("images", CustomImagesAPIViewSet)
+api_router.register_endpoint("documents", CustomDocumentsAPIViewSet)
