7279057edea4676e1fdfbde1536ed5c90a767a25
Muhammad Arslan Abdul Rauf <arslan.abdulrauf@arbisoft.com>
2025-03-05 16:31:57 +0500

    feat: course list filtered based on page type validation added for is_external field of course if it is already associated with a CoursePage/ExternalCoursePage

---
+++ courses/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+from courses.models import Course
+
+
+class CourseForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = "__all__"
+
+    def clean(self):
+        """
+        Ensures that is_external field is not changed if course is associated with CoursePage or ExternalCoursePage
+        """
+        if 'is_external' in self.changed_data:
+            if getattr(self.instance, "coursepage", None):
+                raise ValidationError(
+                    {'is_external': _('Course is associated with CoursePage, cannot change is_external value')}
+                )
+            elif getattr(self.instance, "externalcoursepage", None):
+                raise ValidationError(
+                    {'is_external': _('Course is associated with ExternalCoursePage, cannot change is_external value')}
+                )
+
+        return super().clean()
