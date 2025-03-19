552949dc3c0d21e4d73bd9645dc3272077954a94
Muhammad Anas <88967643+Anas12091101@users.noreply.github.com>
2025-03-19 07:47:35 -0700

    fix: course run updated_on updation when deactivating external course run (#3411)
    * fix: course run updated_on updation when deactivating external course run
    
    * fix: updated_on for product
    
    * fix: test
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- courses/sync_external_courses/external_course_sync_api.py
+++ courses/sync_external_courses/external_course_sync_api.py
@@ -854,18 +854,19 @@ def deactivate_missing_course_runs(external_course_run_codes, platform):
             Prefetch("product", queryset=Product.all_objects.filter(is_active=True))
         )
     )
-
     for course_run in course_runs:
         if course_run.is_unexpired:
             course_run.live = False
+            course_run.updated_on = now_in_utc()
             deactivated_runs_list.append(course_run.external_course_run_id)
             related_product = course_run.product.first()
             if related_product:
                 related_product.is_active = False
+                related_product.updated_on = now_in_utc()
                 updated_products.append(related_product)
 
-    CourseRun.objects.bulk_update(course_runs, ["live"])
-    Product.objects.bulk_update(updated_products, ["is_active"])
+    CourseRun.objects.bulk_update(course_runs, ["live", "updated_on"])
+    Product.objects.bulk_update(updated_products, ["is_active", "updated_on"])
     log.info(
         f"Deactivated {len(deactivated_runs_list)} course runs for platform {platform.name}."
     )
