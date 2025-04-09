3b45b999a147d5c273d2b7038f5b5bb350a0bb19
Muhammad Anas <muhammad.anas@arbisoft.com>
2025-04-09 18:44:58 +0500

    fix: spreadsheet matching

--- sheets/management/utils.py
+++ sheets/management/utils.py
@@ -22,9 +22,10 @@ def get_assignment_spreadsheet_by_title(pygsheets_client, title):
             base_query=CouponAssignmentHandler.ASSIGNMENT_SHEETS_QUERY, title=title
         )
     )
-    if len(matching_spreadsheets) != 1:
+    exact_match_sheets = [sheet for sheet in matching_spreadsheets if sheet.title == title]
+    if len(exact_match_sheets) != 1:
         raise CouponAssignmentError(
             f"There should be 1 coupon assignment sheet that matches the given title ('{title}'). "  # noqa: EM102
             f"{len(matching_spreadsheets)} were found."
         )
-    return matching_spreadsheets[0]
+    return exact_match_sheets[0]
