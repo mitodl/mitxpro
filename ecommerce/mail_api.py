15ed74f1f73f50fa7731cbb7f5c63f2ccaac2433
asajjad2 <areebsajjad21@gmail.com>
2025-05-12 17:18:40 +0500

    refactor: remove redundant if condition

--- ecommerce/mail_api.py
+++ ecommerce/mail_api.py
@@ -186,7 +186,7 @@ def send_course_run_enrollment_welcome_email(enrollment):
     run_start_date, run_start_time = format_run_date(enrollment.run.start_date)
     run_end_date, run_end_time = format_run_date(enrollment.run.end_date)
     coursepage = getattr(enrollment.run.course, "coursepage", None)
-    run_duration = getattr(coursepage, "max_weeks", None) if coursepage else None
+    run_duration = getattr(coursepage, "max_weeks", None)
 
     try:
         user = enrollment.user
