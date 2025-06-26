1bedbe1ec289a0cc4cbd30f1bf959978a03ffbd4
Areeb Sajjad <areebsajjad21@gmail.com>
2025-06-26 10:36:53 +0500

    refactor: replace withRouter in ScrollToTop with useLocation hook

--- static/js/components/ScrollToTop.js
+++ static/js/components/ScrollToTop.js
@@ -1,16 +1,12 @@
-import React from "react";
-import { withRouter } from "react-router";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
-class ScrollToTop extends React.Component {
-  componentDidUpdate(prevProps) {
-    if (this.props.location.pathname !== prevProps.location.pathname) {
-      window.scrollTo(0, 0);
-    }
-  }
-
-  render() {
-    return this.props.children;
-  }
+function ScrollToTop({ children }) {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children;
 }
 
-export default withRouter(ScrollToTop);
+export default ScrollToTop;
