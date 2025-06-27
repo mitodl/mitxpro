eb74ef45c17923eb18944c8688fafc86c610d087
Areeb Sajjad <areebsajjad21@gmail.com>
2025-06-27 09:35:27 +0500

    refactor: use BrowserRouter from react-router-dom in Router.js

--- static/js/Router.js
+++ static/js/Router.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Route, Router as ReactRouter } from "react-router"
+import { BrowserRouter } from "react-router-dom"
 import { Provider } from "react-redux"
 
 import App from "./containers/App"
@@ -8,19 +8,18 @@ import ScrollToTop from "./components/ScrollToTop"
 
 export default class Root extends React.Component {
   props: {
-    history: Object,
     store: Store
   }
 
   render() {
-    const { children, history, store } = this.props
+    const { children, store } = this.props
 
     return (
       <div>
         <Provider store={store}>
-          <ReactRouter history={history}>
+          <BrowserRouter>
             <ScrollToTop>{children}</ScrollToTop>
-          </ReactRouter>
+          </BrowserRouter>
         </Provider>
       </div>
     )
