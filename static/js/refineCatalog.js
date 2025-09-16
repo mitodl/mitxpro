8b540a514fa6440bbd92366334e707a188cbe891
Asad Ali <asad.ali@arbisoft.com>
2025-09-16 16:51:13 +0500

    fix: update tab value in URL when filter applies on catalog page

--- static/js/refineCatalog.js
+++ static/js/refineCatalog.js
@@ -5,9 +5,12 @@
 export default function refineCatalogCourses(filterClass, filterParamName) {
   $(`.${filterClass}`).on("click", function (event) {
     event.preventDefault();
+    const tabName =
+      document.querySelector(".nav-link.active").parentElement.dataset.tabName;
     const url = new URL(window.location.href);
     const searchParams = url.searchParams;
     searchParams.set(filterParamName, $(event.target).attr("data-value"));
+    searchParams.set("active-tab", tabName);
     url.search = searchParams.toString();
     window.location.href = url.toString();
   });
