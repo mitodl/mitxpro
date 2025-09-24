8bc4f10b4ca96f5b6ca5c5e738d1e35c51e82248
Doof <mitx-devops@mit.edu>
2025-09-24 12:54:48 +0000

    Release 0.185.2

--- mitxpro/settings.py
+++ mitxpro/settings.py
@@ -26,7 +26,7 @@ from redbeat import RedBeatScheduler
 from mitxpro.celery_utils import OffsettingSchedule
 from mitxpro.sentry import init_sentry
 
-VERSION = "0.185.1"
+VERSION = "0.185.2"
 
 env.reset()
 
