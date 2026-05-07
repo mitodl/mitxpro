e66b7a23d4c94611fe18c09be7357007f27cde31
Muhammad Arslan Abdul Rauf <arslan.abdulrauf@arbisoft.com>
2026-05-07 20:01:21 +0500

    fix: user page fixed by overriding wagtail userviewset

---
+++ users/wagtail_views.py
@@ -0,0 +1,28 @@
+"""Wagtail admin views for the users app"""
+
+from wagtail.users.views.users import (
+    IndexView as WagtailUserIndexView,
+    UserViewSet as WagtailUserViewSet,
+)
+
+
+class UserIndexView(WagtailUserIndexView):
+    """
+    Custom IndexView for the mitxpro User model.
+
+    The mitxpro User model uses a single `name` field instead of the standard
+    Django `first_name` / `last_name` fields. Wagtail 6.x's built-in
+    order_queryset() maps ordering="name" to order_by("last_name", "first_name"),
+    which raises a FieldError. This overrides that behaviour.
+    """
+
+    def order_queryset(self, queryset):
+        if self.ordering == "name":
+            return queryset.order_by("name")
+        if self.ordering == "-name":
+            return queryset.order_by("-name")
+        return super().order_queryset(queryset)
+
+
+class UserViewSet(WagtailUserViewSet):
+    index_view_class = UserIndexView
