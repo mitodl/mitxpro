247dbe9d543b0729c6836e58b16a1704070983ec
Arslan Ashraf <34372316+arslanashraf7@users.noreply.github.com>
2026-06-02 18:12:15 +0500

    config: add missing related settings from mitol-django-olposthog package (#3943)
    * config: add missing related settings from mitol-django-olposthog package

--- mitxpro/settings.py
+++ mitxpro/settings.py
@@ -35,6 +35,7 @@ import_settings_modules(
     "mitol.authentication.settings.djoser_settings",
     "mitol.common.settings.webpack",
     "mitol.digitalcredentials.settings",
+    "mitol.olposthog.settings.olposthog",
 )
 
 ENVIRONMENT = get_string(
@@ -1236,43 +1237,6 @@ VOUCHER_INTERNATIONAL_COURSE_NUMBER_KEY = get_string(
 
 VOUCHER_COMPANY_ID = get_int(
     name="VOUCHER_COMPANY_ID", default="1", description="Company ID for vouchers"
-)
-
-# PostHog related settings
-
-POSTHOG_ENABLED = get_bool(
-    name="POSTHOG_ENABLED",
-    default=False,
-    description="Whether PostHog is enabled",
-)
-
-POSTHOG_PROJECT_API_KEY = get_string(
-    name="POSTHOG_PROJECT_API_KEY",
-    default="",
-    description="API token to communicate with PostHog",
-)
-
-POSTHOG_API_HOST = get_string(
-    name="POSTHOG_API_HOST",
-    default="",
-    description="API host for PostHog",
-)
-POSTHOG_FEATURE_FLAG_REQUEST_TIMEOUT_MS = get_int(
-    name="POSTHOG_FEATURE_FLAG_REQUEST_TIMEOUT_MS",
-    default=3000,
-    description="Timeout(MS) for PostHog feature flag requests.",
-)
-
-POSTHOG_MAX_RETRIES = get_int(
-    name="POSTHOG_MAX_RETRIES",
-    default=3,
-    description="Number of times that requests to PostHog should be retried after failing.",
-)
-
-POSTHOG_POLL_INTERVAL = get_int(
-    name="POSTHOG_POLL_INTERVAL",
-    default=300,
-    description="Seconds between PostHog flag config polling. Relevant when POSTHOG_PERSONAL_API_KEY is set for local evaluation.",
 )
 
 # Hubspot sync settings
