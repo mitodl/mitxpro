8601b0a8880e22b3c2a898ce4093d67d0b9ec238
Arslan Ashraf <arslan.ashraf@arbisoft.com>
2026-06-02 16:49:55 +0500

    address feedback

--- mitxpro/settings.py
+++ mitxpro/settings.py
@@ -35,6 +35,7 @@ import_settings_modules(
     "mitol.authentication.settings.djoser_settings",
     "mitol.common.settings.webpack",
     "mitol.digitalcredentials.settings",
+    "mitol.olposthog.settings.olposthog",
 )
 
 ENVIRONMENT = get_string(
@@ -1236,56 +1237,6 @@ VOUCHER_INTERNATIONAL_COURSE_NUMBER_KEY = get_string(
 
 VOUCHER_COMPANY_ID = get_int(
     name="VOUCHER_COMPANY_ID", default="1", description="Company ID for vouchers"
-)
-
-# PostHog related settings
-
-POSTHOG_ENABLED = get_bool(
-    name="POSTHOG_ENABLED",
-    default=False,
-    description="Whether PostHog is enabled",
-)
-
-POSTHOG_PROJECT_API_KEY = get_string(
-    name="POSTHOG_PROJECT_API_KEY",
-    default="",
-    description="API token to communicate with PostHog",
-)
-
-POSTHOG_API_HOST = get_string(
-    name="POSTHOG_API_HOST",
-    default="",
-    description="API host for PostHog",
-)
-
-POSTHOG_FEATURE_FLAG_REQUEST_TIMEOUT_MS = get_int(
-    name="POSTHOG_FEATURE_FLAG_REQUEST_TIMEOUT_MS",
-    default=3000,
-    description="Timeout(MS) for PostHog feature flag requests.",
-)
-
-POSTHOG_MAX_RETRIES = get_int(
-    name="POSTHOG_MAX_RETRIES",
-    default=3,
-    description="Number of times that requests to PostHog should be retried after failing.",
-)
-
-POSTHOG_POLL_INTERVAL = get_int(
-    name="POSTHOG_POLL_INTERVAL",
-    default=300,
-    description="Seconds between PostHog flag config polling. Relevant when POSTHOG_PERSONAL_API_KEY is set for local evaluation.",
-)
-
-POSTHOG_CIRCUIT_BREAKER_COOLDOWN_SECONDS = get_int(
-    name="POSTHOG_CIRCUIT_BREAKER_COOLDOWN_SECONDS",
-    default=60,
-    description="Seconds to wait before retrying PostHog after a failed request.",
-)
-
-POSTHOG_CIRCUIT_BREAKER_TRIP_THRESHOLD_SECONDS = get_int(
-    name="POSTHOG_CIRCUIT_BREAKER_TRIP_THRESHOLD_SECONDS",
-    default=6,
-    description="Seconds a PostHog request can take before the circuit breaker trips.",
 )
 
 # Hubspot sync settings
