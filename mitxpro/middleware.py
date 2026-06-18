4647e13834ecf6d463979b5158e3907f71848c08
Asad Ali <asad.ali@arbisoft.com>
2026-06-18 18:34:13 +0500

    chore: add basic logging in redirect middleware (#3966)
    * chore: add basic logging in redirect middleware
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * refactor
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- mitxpro/middleware.py
+++ mitxpro/middleware.py
@@ -1,9 +1,12 @@
 """Middleware for MIT xPRO"""
 
+import logging
 from urllib.parse import urlparse
 
 from django.conf import settings
 from django.http import HttpResponseRedirect
+
+log = logging.getLogger(__name__)
 
 
 class HostnameRedirectMiddleware:
@@ -21,14 +24,24 @@ class HostnameRedirectMiddleware:
         parsed = urlparse(site_base_url)
         canonical_host = parsed.netloc
         canonical_scheme = parsed.scheme
-
+        request_host = request.get_host()
         if (
             not settings.CANONICAL_HOSTNAME_REDIRECT_ENABLED
-            or request.get_host() == canonical_host
+            or request_host == canonical_host
         ):
             return self.get_response(request)
 
-        redirect_url = "{}://{}{}".format(
-            canonical_scheme, canonical_host, request.get_full_path()
+        log.info(
+            "Hostname redirect: scheme=%s host=%s path=%s -> scheme=%s host=%s path=%s "
+            "(forwarded_host=%s forwarded_proto=%s)",
+            request.scheme,
+            request_host,
+            request.path,
+            canonical_scheme,
+            canonical_host,
+            request.path,
+            request.META.get("HTTP_X_FORWARDED_HOST"),
+            request.META.get("HTTP_X_FORWARDED_PROTO"),
         )
+        redirect_url = f"{canonical_scheme}://{canonical_host}{request.get_full_path()}"
         return HttpResponseRedirect(redirect_url)
